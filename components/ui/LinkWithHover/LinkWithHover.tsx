207f9e29d43030f2f9214ab09f58e071a9109db0
alan damian Fernandez <92124271+Alan-Fernandez@users.noreply.github.com>
2024-08-01 21:44:33 -0300

    Términos y Condiciones completo

---
+++ components/ui/LinkWithHover/LinkWithHover.tsx
@@ -0,0 +1,33 @@
+// components/LinkWithHover.tsx
+'use client';
+
+import React, { FC, MouseEvent } from 'react';
+
+interface LinkWithHoverProps {
+    href: string;
+    children: React.ReactNode;
+}
+
+const LinkWithHover: FC<LinkWithHoverProps> = ({ href, children }) => {
+    const handleMouseOver = (e: MouseEvent<HTMLAnchorElement>) => {
+        e.currentTarget.style.textDecoration = 'underline';
+    };
+
+    const handleMouseOut = (e: MouseEvent<HTMLAnchorElement>) => {
+        e.currentTarget.style.textDecoration = 'none';
+    };
+
+    return (
+        <a
+            href={href}
+            target="_blank"
+            style={{ color: '#012a8d', textDecoration: 'none' }}
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
+        >
+            {children}
+        </a>
+    );
+};
+
+export default LinkWithHover;
