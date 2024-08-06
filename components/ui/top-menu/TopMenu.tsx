7c78ec49818c26855802bc39115fe3aa36720369
Ludmila <lurm98@gmail.com>
2024-08-06 13:48:53 -0300

    styles

--- components/ui/top-menu/TopMenu.tsx
+++ components/ui/top-menu/TopMenu.tsx
@@ -8,26 +8,36 @@ import styled from '@emotion/styled';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
 const MenuIcon = styled(GiHamburgerMenu)`
-    width: 30px;
-    height: 30px;
-    color: #333;
-    cursor: pointer;
+  width: 30px;
+  height: 30px;
+  color: #333;
+  cursor: pointer;
 
-    &:hover {
-        color: #007BFF;
-    }
+  &:hover {
+    color: #007BFF;
+  }
 `;
 
 const StyledNavbar = styled(Navbar)`
   background-color: #ffffff;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  padding: 1rem 2rem;
+  height: 100px;
+  padding: 1.5rem;
   font-family: 'Helvetica Neue', Arial, sans-serif;
+  width: 100%;
+
+
+  .navbar-content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 75%;
+    margin: 0 auto; 
+  }
 
   .navbar-brand {
     display: flex;
     align-items: center;
-    margin-left: 2em; 
   }
 
   .navbar-collapse {
@@ -49,47 +59,49 @@ const StyledNavbar = styled(Navbar)`
 
 export function TopMenu() {
   return (
-    <StyledNavbar fluid rounded>
-      <Navbar.Brand href="/">
-        <Image
-          alt="Your Company"
-          src="https://res.cloudinary.com/df5jwzuq9/image/upload/v1722209853/logo_g74htq.png"
-          width={200}
-          height={200}
-          className="h-12 w-auto navbar-brand"
-        />
-      </Navbar.Brand>
-      <div className="flex items-center">
-        <div className="flex md:hidden">
-          <Dropdown
-            arrowIcon={false}
-            inline
-            label={<MenuIcon />}
-          >
-            <Dropdown.Item >
-              <Link href="/info/about-us">Quienes Somos</Link>
-            </Dropdown.Item>
-            <Dropdown.Item>
-              <Link href="/info/how-to-use">Como Usar Swaplyar</Link>
-            </Dropdown.Item>
-            <Dropdown.Item>
-              <Link href="/info/loyalty-program">Programa de Fidelizacion</Link>
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item>
-              <Link href="/auth/login">Login</Link>
-            </Dropdown.Item>
-            <Dropdown.Item>
-              <Link href="/auth/new-account" >Register</Link>
-            </Dropdown.Item>
-          </Dropdown>
-        </div>
-
-        <Navbar.Collapse>
-          <div className="hidden md:flex md:ml-6">
-            <NavLinks />
+    <StyledNavbar fluid rounded >
+      <div className="navbar-content">
+        <Navbar.Brand href="/">
+          <Image
+            alt="Your Company"
+            src="https://res.cloudinary.com/df5jwzuq9/image/upload/v1722209853/logo_g74htq.png"
+            width={200}
+            height={200}
+            className="h-12 w-auto navbar-brand"
+          />
+        </Navbar.Brand>
+        <div className="flex items-center">
+          <div className="flex md:hidden">
+            <Dropdown
+              arrowIcon={false}
+              inline
+              label={<MenuIcon />}
+            >
+              <Dropdown.Item>
+                <Link href="/info/about-us">Quienes Somos</Link>
+              </Dropdown.Item>
+              <Dropdown.Item>
+                <Link href="/info/how-to-use">Como Usar Swaplyar</Link>
+              </Dropdown.Item>
+              <Dropdown.Item>
+                <Link href="/info/loyalty-program">Programa de Fidelizacion</Link>
+              </Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item>
+                <Link href="/auth/login">Login</Link>
+              </Dropdown.Item>
+              <Dropdown.Item>
+                <Link href="/auth/new-account">Register</Link>
+              </Dropdown.Item>
+            </Dropdown>
           </div>
-        </Navbar.Collapse>
+
+          <Navbar.Collapse>
+            <div className="hidden md:flex md:ml-6">
+              <NavLinks />
+            </div>
+          </Navbar.Collapse>
+        </div>
       </div>
     </StyledNavbar>
   );
