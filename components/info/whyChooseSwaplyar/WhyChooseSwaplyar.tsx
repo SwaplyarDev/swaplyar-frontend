c5e11b260916bc8ac7acba29bd513fc17cae917d
alan damian Fernandez <92124271+Alan-Fernandez@users.noreply.github.com>
2024-08-12 10:01:22 -0300

    page creation Why Choose SwaplyAr?

--- components/info/whyChooseSwaplyar/WhyChooseSwaplyar.tsx
+++ components/info/whyChooseSwaplyar/WhyChooseSwaplyar.tsx
@@ -1,7 +1,7 @@
 import FlyerTrabajo from "@/components/FlyerTrabajo/FlyerTrabajo";
 import InfoBlock from "@/components/InfoBlock/InfoBlock";
-import { CentroDeAyuda, FlyerGif, RecargaPaypal, UsdArs } from "@/utils/assets/img-database";
-
+import GuaranteeSection from "@/components/ui/warranty-section/WarrantySection";
+import { CentroDeAyuda, FlyerGif } from "@/utils/assets/img-database";
 
 const mainStyles = {
     main: "py-10",
@@ -9,54 +9,84 @@ const mainStyles = {
     instructionsCalculatorContainer: "flex space-x-4", 
 };
 
-const WhyChooseSwaplyar = () => {
-  return (
- <main className={mainStyles.main}>
-            <FlyerTrabajo
-                imageSrc={CentroDeAyuda}
-            >
-                Estamos trabajando en las funciones de inicio de sesión y registro.
-            </FlyerTrabajo>
-            <div className={mainStyles.instructionsCalculatorContainer}>
-                {/* <ConversionInstructions /> */}
-            </div>
-            <div className={mainStyles.infoBlocksContainer}>
-                <InfoBlock
-                    title="Cambia USD de PayPal por ARS"
-                    imageSrc="/images/Apoyo.png"
-                    imageAlt="Cambia USD de PayPal por ARS"
-                    content="Realizá cambios de dólares de PayPal a pesos argentinos de manera rápida y eficiente. Ofrecemos las mejores tasas del mercado para que maximices tus ganancias. Si necesitás transferir dinero desde PayPal, lo depositamos directamente en tu cuenta bancaria local o internacional según prefieras. Aumentá tus beneficios con SwaplyAr."
-                />
-                <InfoBlock
-                    title="Cambia USD de PayPal por ARS"
-                    imageSrc="/images/Transparencia.png"
-                    imageAlt="Cambia USD de PayPal por ARS"
-                    content="Realizá cambios de dólares de PayPal a pesos argentinos de manera rápida y eficiente. Ofrecemos las mejores tasas del mercado para que maximices tus ganancias. Si necesitás transferir dinero desde PayPal, lo depositamos directamente en tu cuenta bancaria local o internacional según prefieras. Aumentá tus beneficios con SwaplyAr."
-                />
-                <InfoBlock
-                    title="Necesitás recargar tu cuenta de PayPal en USD o EUR, fácilmente en SwaplyAr tenés la solución"
-                    imageSrc="/images/Simplicidad.png"
-                    imageAlt="Recarga PayPal"
-                    content="Simplemente envianos un mensaje especificando la cantidad que necesitás y te proporcionaremos una cotización. Si aceptás el precio, procederemos con la transacción de manera rápida y segura."
-                />
-                <InfoBlock
-                    title="Necesitás recargar tu cuenta de PayPal en USD o EUR, fácilmente en SwaplyAr tenés la solución"
-                    imageSrc="/images/Simplicidad.png"
-                    imageAlt="Recarga PayPal"
-                    content="Simplemente envianos un mensaje especificando la cantidad que necesitás y te proporcionaremos una cotización. Si aceptás el precio, procederemos con la transacción de manera rápida y segura."
-                />
-            </div>
-            <div 
-                className="mt-10"
-            >
-                <FlyerTrabajo
-                imageSrc={FlyerGif}
-                >
-                    &iquest;Nuevo en SwaplyAr? Hac&eacute; clic en &quot;C&oacute;mo usar SwaplyAr&quot; y aprend&eacute; a operar f&aacute;cilmente. &iexcl;Empez&aacute; ahora!
-                </FlyerTrabajo>
-            </div>
-        </main>
-  )
-}
+const WhyChooseSwaplyar: React.FC = () => {
+    return (
+        <main className={mainStyles.main}>
+        <FlyerTrabajo imageSrc={CentroDeAyuda}>
+            Estamos trabajando en las funciones de inicio de sesión y registro.
+        </FlyerTrabajo>
+        
+        <div className={mainStyles.instructionsCalculatorContainer}>
+            <GuaranteeSection
+            title="¿Por Qué Elegir SwaplyAr para Tu Cambio de Divisas?"
+            imageSrc="/images/elegirSwaplyAr.png"
+            imageAlt="¿Por Qué Elegir SwaplyAr para Tu Cambio de Divisas?"
+            contentNode={(
+                <>
+                    <p>
+                        Si estás buscando seguridad y confiabilidad en el intercambio de tu
+                        dinero digital, SwaplyAr es tu mejor opción. Descubre por qué somos la
+                        elección preferida para cambiar divisas de PayPal, con las mejores
+                        tasas del mercado y una plataforma fácil de usar.
+                    </p>
+                </>
+            )}
+            />
+        </div>
 
-export default WhyChooseSwaplyar
+        <div className={mainStyles.infoBlocksContainer}>
+            <InfoBlock
+            title="¿Por qué elegirnos?"
+            imageSrc="/images/porqueelegirnos.png"
+            imageAlt="Cambia USD de PayPal por ARS"
+            contentNode={(
+                <>
+                    <p>Las razones para elegirnos incluyen:</p>
+                    <ol className="list-decimal ml-4">
+                        <li>Más rápido, más fácil y la mejor tasa del mercado.</li>
+                        <li>Los Pedidos son completados en menos de 1 hora.</li>
+                        <li>Soporte mediante Chat en WhatsApp.</li>
+                        <li>Seguridad.</li>
+                        <li>Actualización diaria con tasas competitivas.</li>
+                        <li>Descuentos para clientes activos.</li>
+                        <li>Servicio 24/7.</li>
+                    </ol>
+                </>
+            )}
+            />
+
+            <InfoBlock
+            title="SwaplyAr: Líderes en Cambio de Divisas de PayPal"
+            imageSrc="/images/lideresencambio.png"
+            imageAlt="Recarga PayPal"
+            content="
+                Orgullosos de ofrecer las mejores tasas del mercado, en SwaplyAr nos
+                avalan más de 1,500 usuarios satisfechos. El 90% nos recomienda por
+                nuestra plataforma segura y fácil de usar. Únete y maximiza tu dinero
+                digital con confianza.
+            "
+            />
+
+            <InfoBlock
+            title="Tu Seguridad y Satisfacción Son Nuestra Prioridad en SwaplyAr"
+            imageSrc="/images/seguridadysatisfaccion.png"
+            imageAlt="Recarga PayPal"
+            content="
+                Con nosotros, tu dinero y tú están en manos seguras. Nuestro dedicado
+                equipo de soporte al cliente está disponible en todo momento para
+                asegurar que cualquier duda o inconveniente sea resuelto rápidamente,
+                garantizando una experiencia sin estrés.
+            "
+            />
+        </div>
+
+        <div className="mt-10">
+            <FlyerTrabajo imageSrc={FlyerGif}>
+            &iquest;Nuevo en SwaplyAr? Hac&eacute; clic en &quot;C&oacute;mo usar SwaplyAr&quot; y aprend&eacute; a operar f&aacute;cilmente. &iexcl;Empez&aacute; ahora!
+            </FlyerTrabajo>
+        </div>
+        </main>
+    );
+};
+
+export default WhyChooseSwaplyar;
