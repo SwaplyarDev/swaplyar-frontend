fd90145f2d3292d7b5136abbd2aba2cc52c6a8fc
CarlosLagos29 <137452483+CarlosLagos29@users.noreply.github.com>
2024-08-21 11:25:15 -0300

    Cambios en la visualización en el modo tablet
    se cambio el como se ve la nav bar en el modo de tablets centrando el logo moviendo el menú a la izquierda y agregando un botón de inicio de sesión a la derecha

--- components/ui/top-menu/TopMenu.tsx
+++ components/ui/top-menu/TopMenu.tsx
@@ -11,10 +11,13 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 import useStore from '@/store/store';
 import { useDarkTheme } from '@/components/ui/theme-Provider/themeProvider';
 import styles from './switchStyle.module.css';
+import { usePathname } from 'next/navigation';
 
 export function TopMenu() {
   const [selectedItem, setSelectedItem] = useState<string | null>(null);
   const { setView } = useStore();
+
+  const pathname = usePathname();
 
   const handleSelect = (item: string) => {
     setSelectedItem(item);
@@ -23,109 +26,126 @@ export function TopMenu() {
   const handleChange = () => {
     setView('register');
   };
+
   const { changeTheme, isDark } = useDarkTheme();
 
   return (
-    <Navbar fluid rounded className="bg-white p-4 shadow-md dark:bg-gray-800">
-      <Navbar.Brand href="/">
-        <Image
-          alt="Your Company"
-          src="https://res.cloudinary.com/df5jwzuq9/image/upload/v1722209853/logo_g74htq.png"
-          width={200}
-          height={200}
-          className="h-12 w-auto filter dark:brightness-[300%]"
-        />
-      </Navbar.Brand>
-      <div className="flex items-center justify-center">
-
-        <section className="flex items-center justify-end">
-          <label className={`${styles.switch} m-3 lg:-mr-5 `}>
-            <input type="checkbox" onClick={changeTheme} />
-            <span className={isDark ? styles.sliderDark : styles.sliderLight}></span>
-          </label>
-        </section>
-
-        <div className="flex lg:hidden">
-          <Dropdown
-            arrowIcon={false}
-            inline
-            label={
-              <GiHamburgerMenu className="h-8 w-8 text-gray-800 dark:text-gray-200" />
-            }
+    <Navbar fluid rounded className="p-4 shadow-md">
+      <div className="flex w-full flex-row-reverse justify-between lg:flex-row">
+        <Link
+          key="Iniciar sesión"
+          href="/auth/login-register"
+          onClick={() => setView('login')}
+          className={`hidden relative md:flex lg:hidden h-[48px] items-center gap-2 rounded-md p-3 ${pathname === '/auth/login-register' ? 'border-2 border-sky-200 bg-gray-500 text-white' : 'text-gray-900'} m-1 transition-colors duration-300 ease-in-out hover:bg-gray-600 hover:text-white hover:shadow-sm dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700`}
+        >
+          <p
+            className="font-bold"
           >
-            <Dropdown.Item
-              className={`cursor-pointer ${
-                selectedItem === 'about-us'
-                  ? 'bg-blue-500 text-white'
-                  : 'hover:bg-gray-100 dark:hover:bg-gray-600'
-              }`}
-              onClick={() => handleSelect('about-us')}
-            >
-              <Link href="/info/about-us">Quienes Somos</Link>
-            </Dropdown.Item>
-            <Dropdown.Item
-              className={`cursor-pointer ${
-                selectedItem === 'how-to-use'
-                  ? 'bg-blue-500 text-white'
-                  : 'hover:bg-gray-100 dark:hover:bg-gray-600'
-              }`}
-              onClick={() => handleSelect('how-to-use')}
-            >
-              <Link href="/info/how-to-use">Como Usar Swaplyar</Link>
-            </Dropdown.Item>
-            <Dropdown.Item
-              className={`cursor-pointer ${
-                selectedItem === 'loyalty-program'
-                  ? 'bg-blue-500 text-white'
-                  : 'hover:bg-gray-100 dark:hover:bg-gray-600'
-              }`}
-              onClick={() => handleSelect('loyalty-program')}
-            >
-              <Link href="/info/loyalty-program">Programa de Fidelización</Link>
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <section className='md:hidden'>
+            Iniciar sesión
+          </p>
+        </Link>
 
-            <Dropdown.Item
-              className={`cursor-pointer ${
-                selectedItem === 'login'
-                  ? 'bg-blue-500 text-white'
-                  : 'hover:bg-gray-100 dark:hover:bg-gray-600'
-              }`}
-              onClick={() => handleSelect('login')}
+        <Navbar.Brand href="/">
+          <Image
+            alt="Your Company"
+            src="https://res.cloudinary.com/df5jwzuq9/image/upload/v1722209853/logo_g74htq.png"
+            width={200}
+            height={200}
+            className="h-12 w-auto filter dark:brightness-[300%]"
+          />
+        </Navbar.Brand>
+        <nav className="flex flex-row-reverse items-center justify-center lg:flex-row">
+          <section className="flex items-center justify-end">
+            <label className={`${styles.switch} ml-3`}>
+              <input type="checkbox" onClick={changeTheme} />
+              <span
+                className={isDark ? styles.sliderDark : styles.sliderLight}
+              ></span>
+            </label>
+          </section>
+
+          <section className="flex lg:hidden">
+            <Dropdown
+              arrowIcon={false}
+              inline
+              label={
+                <GiHamburgerMenu className="size-8 text-gray-800 dark:text-gray-200" />
+              }
             >
-              <Link
-                href="/auth/login-register"
-                onClick={() => setView('login')}
+              <Dropdown.Item
+                className={`cursor-pointer ${
+                  selectedItem === 'about-us'
+                    ? 'bg-blue-500 text-white'
+                    : 'hover:bg-gray-100 dark:hover:bg-gray-600'
+                }`}
+                onClick={() => handleSelect('about-us')}
               >
-                Login
-              </Link>
-            </Dropdown.Item>
-            <Dropdown.Item
-              className={`cursor-pointer ${
-                selectedItem === 'register'
-                  ? 'bg-blue-500 text-white'
-                  : 'hover:bg-gray-100 dark:hover:bg-gray-600'
-              }`}
-              onClick={() => handleSelect('register')}
-            >
-              <Link
-                href="/auth/login-register"
-                onClick={() => setView('register')}
+                <Link href="/info/about-us">Quienes Somos</Link>
+              </Dropdown.Item>
+              <Dropdown.Item
+                className={`cursor-pointer ${
+                  selectedItem === 'how-to-use'
+                    ? 'bg-blue-500 text-white'
+                    : 'hover:bg-gray-100 dark:hover:bg-gray-600'
+                }`}
+                onClick={() => handleSelect('how-to-use')}
               >
-                Register
-              </Link>
-            </Dropdown.Item>
+                <Link href="/info/how-to-use">Como Usar Swaplyar</Link>
+              </Dropdown.Item>
+              <Dropdown.Item
+                className={`cursor-pointer ${
+                  selectedItem === 'loyalty-program'
+                    ? 'bg-blue-500 text-white'
+                    : 'hover:bg-gray-100 dark:hover:bg-gray-600'
+                }`}
+                onClick={() => handleSelect('loyalty-program')}
+              >
+                <Link href="/info/loyalty-program">
+                  Programa de Fidelización
+                </Link>
+              </Dropdown.Item>
+              <Dropdown.Divider />
+              <section className="md:hidden">
+                <Dropdown.Item
+                  className={`cursor-pointer ${
+                    selectedItem === 'login'
+                      ? 'bg-blue-500 text-white'
+                      : 'hover:bg-gray-100 dark:hover:bg-gray-600'
+                  }`}
+                  onClick={() => handleSelect('login')}
+                >
+                  <Link
+                    href="/auth/login-register"
+                    onClick={() => setView('login')}
+                  >
+                    Login
+                  </Link>
+                </Dropdown.Item>
+                <Dropdown.Item
+                  className={`cursor-pointer ${
+                    selectedItem === 'register'
+                      ? 'bg-blue-500 text-white'
+                      : 'hover:bg-gray-100 dark:hover:bg-gray-600'
+                  }`}
+                  onClick={() => handleSelect('register')}
+                >
+                  <Link
+                    href="/auth/login-register"
+                    onClick={() => setView('register')}
+                  >
+                    Register
+                  </Link>
+                </Dropdown.Item>
+              </section>
+            </Dropdown>
+          </section>
+
+          <Navbar.Collapse>
+            <section className="hidden lg:flex">
+              <NavLinks />
             </section>
-          </Dropdown>
-
-        </div>
-
-        <Navbar.Collapse>
-          <div className="hidden lg:ml-6 lg:flex">
-            <NavLinks />
-          </div>
-        </Navbar.Collapse>
+          </Navbar.Collapse>
+        </nav>
       </div>
     </Navbar>
   );
