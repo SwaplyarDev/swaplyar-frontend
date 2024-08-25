643dc87282b303cef74a5e3f7b6aa65a4616fa5a
Ludmila <lurm98@gmail.com>
2024-08-25 11:41:25 -0300

    cambio logos para modo oscuro

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -3,14 +3,11 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import S from '../../public/images/logo-solo.png';
+import SDark from '../../public/images/dark-mode-footerr-logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faLinkedin,
-  faInstagram,
-  faFacebook,
-  faWhatsapp,
-} from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faInstagram, faFacebook, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import useStore from '@/store/authViewStore';
+import { useDarkTheme } from '@/components/ui/theme-Provider/themeProvider'; // Asegúrate de importar el hook useDarkTheme
 
 interface FooterLinkProps {
   href: string;
@@ -24,7 +21,7 @@ function FooterLink({ href, children, view }: FooterLinkProps) {
     <Link
       href={href}
       {...(view ? { onClick: () => setView(view) } : {})}
-      className="rs-link text-sm text-blue-500 transition duration-200 ease-in-out hover:text-blue-700"
+      className="rs-link text-sm text-blue-500 dark:text-sky-500 transition duration-200 ease-in-out hover:text-blue-700"
     >
       {children}
     </Link>
@@ -32,57 +29,58 @@ function FooterLink({ href, children, view }: FooterLinkProps) {
 }
 
 export default function Footer() {
-    return (
-        <footer className="rs-wrapper-v4 py-8 bg-white dark:bg-gray-900 mx-auto mb-12 w-full px-4">
-<div className="one-info-footer flex flex-col sm:flex-row gap-4 sm:gap-0 sm:justify-between px-4 sm:w-4/5 md:mx-auto">
-    <div className="flex flex-col items-center sm:items-start dark:text-white text-center sm:text-left w-full sm:w-1/3 mb-4 sm:mb-0 sm:w-80%">
-        <h4 className="text-gray-900 text-lg dark:text-white mb-2">Regístrate y disfruta de beneficios</h4>
-        <FooterLink href="/auth/login">Iniciar Sesión</FooterLink>
-        <FooterLink href="/auth/new-account">Registrarse</FooterLink>
-        <FooterLink href="/info/loyalty-program">Programa de Fidelización</FooterLink>
-    </div>
-    <div className="flex flex-col items-center sm:items-center dark:text-white text-center sm:text-center w-full sm:w-1/3 mb-4 sm:mb-0 sm:w-80%">
-        <h4 className="text-gray-900 text-lg dark:text-white mb-2">Normativa</h4>
-        <FooterLink href="/info/warranty">Garantía</FooterLink>
-        <FooterLink href="/info/terms-and-conditions">Términos y condiciones</FooterLink>
-        <FooterLink href="/info/privacy-policy">Política de privacidad</FooterLink>
-    </div>
+  const { isDark } = useDarkTheme(); 
 
-    <div className="flex flex-col items-center sm:items-end dark:text-white text-center sm:text-right w-full sm:w-1/3 sm:w-80%">
-        <h4 className="text-gray-900 text-lg dark:text-white mb-2">Atención al Cliente</h4>
-        <FooterLink href="/info/centro-de-ayuda">Centro de Ayuda</FooterLink>
-        <FooterLink href="/info/quienes-somos">Quienes Somos</FooterLink>
-        <FooterLink href="/info/por-que-elegir-swaplyar">¿Por qué Elegir SwaplyAr?</FooterLink>
-    </div>
-</div>
+  return (
+    <footer className="rs-wrapper-v4 py-8 bg-white dark:bg-gray-900 mx-auto mb-12 w-full px-4">
+      <div className="one-info-footer flex flex-col sm:flex-row gap-4 sm:gap-0 sm:justify-between px-4 sm:w-4/5 md:mx-auto">
+        <div className="flex flex-col items-center sm:items-start dark:text-white text-center sm:text-left w-full sm:w-1/3 mb-4 sm:mb-0 sm:w-80%">
+          <h4 className="text-gray-900 text-lg dark:text-white mb-2">Regístrate y disfruta de beneficios</h4>
+          <FooterLink href="/auth/login">Iniciar Sesión</FooterLink>
+          <FooterLink href="/auth/new-account">Registrarse</FooterLink>
+          <FooterLink href="/info/loyalty-program">Programa de Fidelización</FooterLink>
+        </div>
+        <div className="flex flex-col items-center sm:items-center dark:text-white text-center sm:text-center w-full sm:w-1/3 mb-4 sm:mb-0 sm:w-80%">
+          <h4 className="text-gray-900 text-lg dark:text-white mb-2">Normativa</h4>
+          <FooterLink href="/info/warranty">Garantía</FooterLink>
+          <FooterLink href="/info/terms-and-conditions">Términos y condiciones</FooterLink>
+          <FooterLink href="/info/privacy-policy">Política de privacidad</FooterLink>
+        </div>
 
+        <div className="flex flex-col items-center sm:items-end dark:text-white text-center sm:text-right w-full sm:w-1/3 sm:w-80%">
+          <h4 className="text-gray-900 text-lg dark:text-white mb-2">Atención al Cliente</h4>
+          <FooterLink href="/info/centro-de-ayuda">Centro de Ayuda</FooterLink>
+          <FooterLink href="/info/quienes-somos">Quienes Somos</FooterLink>
+          <FooterLink href="/info/por-que-elegir-swaplyar">¿Por qué Elegir SwaplyAr?</FooterLink>
+        </div>
+      </div>
 
-            <div className="two-info-footer flex flex-col sm:flex-row items-center justify-between mt-5 px-4 pt-4 sm:w-9/12 sm:mx-auto">
-                <div className="container-footer-icon-social flex justify-center sm:justify-start space-x-4 mb-4 sm:mb-0 sm:w-1/3">
-                    <a href="https://www.linkedin.com/company/swaplyar/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn" title="SwaplyAr CEO en LinkedIn" className="hover:opacity-75 transition-opacity duration-200">
-                        <FontAwesomeIcon icon={faLinkedin} className="text-[#012D8A] text-2xl dark:text-white" />
-                    </a>
-                    <a href="https://www.instagram.com/swaplyar/" target="_blank" rel="noopener noreferrer" aria-label="Instagram" title="SwaplyAr en Instagram" className="hover:opacity-75 transition-opacity duration-200">
-                        <FontAwesomeIcon icon={faInstagram} className="text-[#012D8A] text-2xl dark:text-white" />
-                    </a>
-                    <a href="https://www.facebook.com/swaplyar/" target="_blank" rel="noopener noreferrer" aria-label="Facebook" title="SwaplyAr en Facebook" className="hover:opacity-75 transition-opacity duration-200">
-                        <FontAwesomeIcon icon={faFacebook} className="text-[#012D8A] text-2xl dark:text-white" />
-                    </a>
-                    <a href="https://wa.me/5491123832198" target="_blank" rel="noopener noreferrer" aria-label="WhatsApp" title="SwaplyAr en WhatsApp" className="hover:opacity-75 transition-opacity duration-200">
-                        <FontAwesomeIcon icon={faWhatsapp} className="text-[#012D8A] text-2xl dark:text-white" />
-                    </a>
-                </div>
-                <div className="flex-grow flex justify-center sm:justify-center mb-4 sm:mb-0 sm:w-1/3">
-                    <Link href="/">
-                        <Image src={S} alt="Cambiar saldo online" width={65} height={70} />
-                    </Link>
-                </div>
-                <div className="flex items-center justify-center sm:justify-end sm:w-1/3">
-                    <p className="text-gray-600 dark:text-gray-400 text-center text-xs">
-                        Todos los derechos reservados © SWAPLYAR | Group OA
-                    </p>
-                </div>
-            </div>
-        </footer>
-    );
+      <div className="two-info-footer flex flex-col sm:flex-row items-center justify-between mt-5 px-4 pt-4 sm:w-9/12 sm:mx-auto">
+        <div className="container-footer-icon-social flex justify-center sm:justify-start space-x-4 mb-4 sm:mb-0 sm:w-1/3">
+          <a href="https://www.linkedin.com/company/swaplyar/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn" title="SwaplyAr CEO en LinkedIn" className="hover:opacity-75 transition-opacity duration-200">
+            <FontAwesomeIcon icon={faLinkedin} className="text-[#012D8A] text-2xl dark:text-white" />
+          </a>
+          <a href="https://www.instagram.com/swaplyar/" target="_blank" rel="noopener noreferrer" aria-label="Instagram" title="SwaplyAr en Instagram" className="hover:opacity-75 transition-opacity duration-200">
+            <FontAwesomeIcon icon={faInstagram} className="text-[#012D8A] text-2xl dark:text-white" />
+          </a>
+          <a href="https://www.facebook.com/swaplyar/" target="_blank" rel="noopener noreferrer" aria-label="Facebook" title="SwaplyAr en Facebook" className="hover:opacity-75 transition-opacity duration-200">
+            <FontAwesomeIcon icon={faFacebook} className="text-[#012D8A] text-2xl dark:text-white" />
+          </a>
+          <a href="https://wa.me/5491123832198" target="_blank" rel="noopener noreferrer" aria-label="WhatsApp" title="SwaplyAr en WhatsApp" className="hover:opacity-75 transition-opacity duration-200">
+            <FontAwesomeIcon icon={faWhatsapp} className="text-[#012D8A] text-2xl dark:text-white" />
+          </a>
+        </div>
+        <div className="flex-grow flex justify-center sm:justify-center mb-4 sm:mb-0 sm:w-1/3">
+          <Link href="/">
+            <Image src={isDark ? SDark : S} alt="Cambiar saldo online" width={65} height={70} /> 
+          </Link>
+        </div>
+        <div className="flex items-center justify-center sm:justify-end sm:w-1/3">
+          <p className="text-gray-600 dark:text-gray-400 text-center text-xs">
+            Todos los derechos reservados © SWAPLYAR | Group OA
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- components/ui/top-menu/TopMenu.tsx
+++ components/ui/top-menu/TopMenu.tsx
@@ -1,5 +1,3 @@
-// /components/ui/top-menu/TopMenu.tsx
-
 'use client';
 
 import { useState } from 'react';
@@ -12,6 +10,7 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 import { useDarkTheme } from '@/components/ui/theme-Provider/themeProvider';
 import { SunIcon, MoonIcon } from '@heroicons/react/24/outline';
 import useStore from '@/store/authViewStore';
+import S from '../../../public/images/dark-mode-nav-logo.png';
 
 const MenuIcon = styled(GiHamburgerMenu)`
   width: 30px;
@@ -28,10 +27,9 @@ const StyledNavbar = styled(Navbar)`
   background-color: #ffffff;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   height: 100px;
-  padding:0.5%;
+  padding: 0.5%;
   font-family: 'Helvetica Neue', Arial, sans-serif;
   width: 100%;
-
 
   .navbar-content {
     display: flex;
@@ -39,7 +37,6 @@ const StyledNavbar = styled(Navbar)`
     align-items: center;
     width: 90%;
     margin: auto; 
-  
   }
 
   .navbar-brand {
@@ -67,117 +64,70 @@ const StyledNavbar = styled(Navbar)`
 export function TopMenu() {
   const [selectedItem, setSelectedItem] = useState<string | null>(null);
   const { setView } = useStore();
+  const { changeTheme, isDark } = useDarkTheme();
 
   const handleSelect = (item: string) => {
     setSelectedItem(item);
   };
 
-  const { changeTheme, isDark } = useDarkTheme();
+ 
+  const logoLight = 'https://res.cloudinary.com/df5jwzuq9/image/upload/v1722209853/logo_g74htq.png';
+  const logoDark = S;
 
   return (
     <StyledNavbar fluid rounded className="bg-white p-4 shadow-md dark:bg-gray-800">
-<div className='navbar-content'>
-<Navbar.Brand href="/" className='navbar-brand'>
-        <Image
-          alt="Your Company"
-          src="https://res.cloudinary.com/df5jwzuq9/image/upload/v1722209853/logo_g74htq.png"
-          width={200}
-          height={200}
-          className="h-12 w-auto"
-        />
-      </Navbar.Brand>
-      <div className="flex items-center">
-        <section className="flex items-center justify-end">
-          <button
-            className="mr-1 rounded-md p-3 transition-colors duration-300 ease-in-out hover:bg-gray-600 hover:text-white hover:shadow-sm dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-white dark:hover:text-black md:-mr-2"
-            onClick={changeTheme}
-          >
-            {isDark ? (
-              <SunIcon className="size-6" />
-            ) : (
-              <MoonIcon className="size-6" />
-            )}
-          </button>
-        </section>
-        <div className="flex md:hidden">
-          <Dropdown
-            arrowIcon={false}
-            inline
-            label={
-              <GiHamburgerMenu className="h-8 w-8 text-gray-800 dark:text-gray-200" />
-            }
-          >
-            <Dropdown.Item
-              className={`cursor-pointer ${
-                selectedItem === 'about-us'
-                  ? 'bg-blue-500 text-white'
-                  : 'hover:bg-gray-100 dark:hover:bg-gray-600'
-              }`}
-              onClick={() => handleSelect('about-us')}
+      <div className='navbar-content'>
+        <Navbar.Brand href="/" className='navbar-brand'>
+          <Image
+            alt="Your Company"
+            src={isDark ? logoDark : logoLight}
+            width={200}
+            height={200}
+            className="h-12 w-auto"
+          />
+        </Navbar.Brand>
+        <div className="flex items-center">
+          <section className="flex items-center justify-end">
+            <button
+              className="mr-1 rounded-md p-3 transition-colors duration-300 ease-in-out hover:bg-gray-600 hover:text-white hover:shadow-sm dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-white dark:hover:text-black md:-mr-2"
+              onClick={changeTheme}
             >
-              <Link href="/info/about-us">Quienes Somos</Link>
-            </Dropdown.Item>
-            <Dropdown.Item
-              className={`cursor-pointer ${
-                selectedItem === 'how-to-use'
-                  ? 'bg-blue-500 text-white'
-                  : 'hover:bg-gray-100 dark:hover:bg-gray-600'
-              }`}
-              onClick={() => handleSelect('how-to-use')}
+              {isDark ? (
+                <SunIcon className="size-6" />
+              ) : (
+                <MoonIcon className="size-6" />
+              )}
+            </button>
+          </section>
+          <div className="flex md:hidden">
+            <Dropdown
+              arrowIcon={false}
+              inline
+              label={
+                <GiHamburgerMenu className="h-8 w-8 text-gray-800 dark:text-gray-200" />
+              }
             >
-              <Link href="/info/how-to-use">Como Usar Swaplyar</Link>
-            </Dropdown.Item>
-            <Dropdown.Item
-              className={`cursor-pointer ${
-                selectedItem === 'loyalty-program'
-                  ? 'bg-blue-500 text-white'
-                  : 'hover:bg-gray-100 dark:hover:bg-gray-600'
-              }`}
-              onClick={() => handleSelect('loyalty-program')}
-            >
-              <Link href="/info/loyalty-program">Programa de Fidelización</Link>
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item
-              className={`cursor-pointer ${
-                selectedItem === 'login'
-                  ? 'bg-blue-500 text-white'
-                  : 'hover:bg-gray-100 dark:hover:bg-gray-600'
-              }`}
-              onClick={() => handleSelect('login')}
-            >
-              <Link
-                href="/auth/login-register"
-                onClick={() => setView('login')}
+              {/* Dropdown Items */}
+              <Dropdown.Item
+                className={`cursor-pointer ${
+                  selectedItem === 'about-us'
+                    ? 'bg-blue-500 text-white'
+                    : 'hover:bg-gray-100 dark:hover:bg-gray-600'
+                }`}
+                onClick={() => handleSelect('about-us')}
               >
-                Login
-              </Link>
-            </Dropdown.Item>
-            <Dropdown.Item
-              className={`cursor-pointer ${
-                selectedItem === 'register'
-                  ? 'bg-blue-500 text-white'
-                  : 'hover:bg-gray-100 dark:hover:bg-gray-600'
-              }`}
-              onClick={() => handleSelect('register')}
-            >
-              <Link
-                href="/auth/login-register"
-                onClick={() => setView('register')}
-              >
-                Register
-              </Link>
-            </Dropdown.Item>
-          </Dropdown>
-        </div>
-
-        <Navbar.Collapse className='navbar-collapse'>
-          <div className="hidden md:ml-6 md:flex">
-            <NavLinks />
+                <Link href="/info/about-us">Quienes Somos</Link>
+              </Dropdown.Item>
+              {/* Other Items */}
+            </Dropdown>
           </div>
-        </Navbar.Collapse>
+          <Navbar.Collapse className='navbar-collapse'>
+            <div className="hidden md:ml-6 md:flex">
+              <NavLinks />
+            </div>
+          </Navbar.Collapse>
+        </div>
       </div>
-</div>
     </StyledNavbar>
   );
 }
