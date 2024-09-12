bd85bee1d75e5b7cc78c444091bd21d3d156bd2d
Ignacio Lopez <nacholopez1903@gmail.com>
2024-09-12 10:28:56 -0300

    implementando carrusel en register-login

--- components/auth/auth-form.tsx
+++ components/auth/auth-form.tsx
@@ -1,21 +1,54 @@
 // components/AuthForm.tsx
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { LoginForm } from './auth-form-login';
 import { RegisterForm } from './auth-form-register';
 import ClientWrapper from '../clientWrapper/ClientWrapper';
 import useStore from '@/store/authViewStore';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Swiper as SwiperType } from 'swiper/types';
 
 const AuthForm: React.FC = () => {
   const { view } = useStore();
 
+  const swiperRef = useRef<SwiperType | null>(null);
+
+  useEffect(() => {
+    if (swiperRef.current && typeof swiperRef.current.slideTo === 'function') {
+      swiperRef.current.slideTo(view === 'login' ? 0 : 1);
+    }
+  }, [view]);
+
+  console.log(view);
+  console.log(swiperRef.current);
+
   return (
     <ClientWrapper>
-      {view === 'login' && <LoginForm />}
-      {view === 'register' && <RegisterForm />}
+      <section className='flex flex-col items-center'>
+        <div className="h-screen w-[512px]">
+          <Swiper
+            onSwiper={(swiper) => (swiperRef.current = swiper)}
+            modules={[Pagination]}
+            spaceBetween={50}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+          >
+            <SwiperSlide>
+              <LoginForm />
+            </SwiperSlide>
+            <SwiperSlide>
+              <RegisterForm />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </section>
     </ClientWrapper>
   );
 };
 
 export default AuthForm;
+
