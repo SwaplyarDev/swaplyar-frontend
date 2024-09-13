52b32b8e00140ef524564183a5b3416358725b42
CarlosLagos29 <137452483+CarlosLagos29@users.noreply.github.com>
2024-09-13 12:30:02 -0300

    Cambios estilos subNavbar

--- components/ui/top-menu/TopMenu.tsx
+++ components/ui/top-menu/TopMenu.tsx
@@ -8,13 +8,15 @@ import useStore from '@/store/authViewStore';
 
 import { Drawer, Sidebar, Navbar } from 'flowbite-react';
 import { GiHamburgerMenu } from 'react-icons/gi';
-import { BsChevronRight } from 'react-icons/bs';
+import { MdOutlineClose } from 'react-icons/md';
 
 import NavLinks from '@/components/ui/top-menu/nav-links';
 import Switch from './switch';
 import Image from 'next/image';
 import Link from 'next/link';
 import S from '../../../public/images/logo-solo.png';
+import style from './sidebar.module.css';
+import { blue } from '@mui/material/colors';
 
 export function TopMenu() {
   const [selectedItem, setSelectedItem] = useState<string | null>(null);
@@ -45,7 +47,6 @@ export function TopMenu() {
     };
   }, []);
 
-
   return (
     <Navbar
       fluid
@@ -56,7 +57,7 @@ export function TopMenu() {
           : 'bg-opacity-100 dark:bg-opacity-100'
       }`}
     >
-      <div className="m-auto flex w-full max-w-screen-2xl flex-row justify-between">
+      <div className="m-auto flex w-[95%] max-w-screen-2xl flex-row justify-between">
         <Link
           key="Iniciar sesión"
           href="/auth/login-register"
@@ -72,20 +73,20 @@ export function TopMenu() {
             src="https://res.cloudinary.com/df5jwzuq9/image/upload/v1722209853/logo_g74htq.png"
             width={200}
             height={200}
-            className="hidden h-12 w-auto rounded-xl filter hover:outline hover:outline-[1px] hover:outline-offset-4 hover:outline-slate-500 dark:brightness-[0%] dark:invert dark:hover:outline-darkText md:block"
+            className="hidden h-12 w-auto rounded-xl filter dark:brightness-[0%] dark:invert md:block"
           />
           <Image
             alt="Your Company"
             src={S}
             width={200}
             height={200}
-            className="h-12 w-auto rounded-xl filter hover:outline hover:outline-[1px] hover:outline-offset-4 hover:outline-slate-500 dark:brightness-[0%] dark:invert dark:hover:outline-darkText md:hidden"
+            className="h-12 w-auto rounded-xl filter dark:brightness-[0%] dark:invert md:hidden"
           />
         </Navbar.Brand>
 
         <nav className="flex flex-row items-center justify-center">
           <section className="flex items-center justify-end gap-5">
-            <Switch/>
+            <Switch />
             <button
               onClick={() => setDrawerMenu(true)}
               className="block lg:hidden"
@@ -98,50 +99,74 @@ export function TopMenu() {
             open={drawerMenu}
             onClose={() => setDrawerMenu(false)}
             position="right"
+            className="transform transition-all duration-500 ease-in-out"
           >
-            <Drawer.Header title="Menu" titleIcon={() => <></>} />
-            <Drawer.Items className="h-[90vh]">
-              <Sidebar aria-label="Sidebar with content separator example">
-                <Sidebar.Items className="flex h-full flex-col justify-between">
-                  <Sidebar.ItemGroup>
+            <Drawer.Header
+              title="Menu"
+              titleIcon={() => <></>}
+              closeIcon={() => <MdOutlineClose className="size-7 text-blue-800 dark:text-sky-500" />}
+              className="flex items-center text-lg font-bold"
+            />
+            <Drawer.Items>
+              <Sidebar
+                aria-label="Sidebar with content separator example"
+                className="m-auto h-[90vh] text-center [&>div]:bg-transparent [&>div]:p-0"
+              >
+                <Sidebar.Items className="flex h-full w-full flex-col justify-between pt-5">
+                  <Sidebar.ItemGroup className="w-full bg-inherit text-left">
                     <Sidebar.Item
-                      className={`cursor-pointer ${
+                      className={`${
                         selectedItem === 'about-us'
-                          ? 'bg-blue-500 text-white'
-                          : 'hover:bg-gray-100 dark:hover:bg-gray-600'
+                          ? 'bg-gray-100 dark:bg-gray-700 h-10'
+                          : 'hover:bg-gray-100 dark:hover:bg-gray-700'
                       }`}
                       onClick={() => handleSelect('about-us')}
                       href="/info/about-us"
                     >
+                      <div
+                        className={`${
+                          selectedItem === 'about-us' ? 'bg-blue-800 dark:bg-sky-500' : ''
+                        } absolute rounded-r-md left-0.5 -mt-2 h-10 w-2`}
+                      ></div>
                       Quienes Somos
                     </Sidebar.Item>
 
                     <Sidebar.Item
-                      className={`cursor-pointer ${
-                        selectedItem === 'how-to-use'
-                          ? 'bg-blue-500 text-white'
-                          : 'hover:bg-gray-100 dark:hover:bg-gray-600'
-                      }`}
+                         className={`${
+                          selectedItem === 'how-to-use'
+                            ? 'bg-gray-100 dark:bg-gray-700 h-10'
+                            : 'hover:bg-gray-100 dark:hover:bg-gray-700'
+                        }`}
                       onClick={() => handleSelect('how-to-use')}
                       href="/info/how-to-use"
                     >
+                       <div
+                        className={`${
+                          selectedItem === 'how-to-use' ? 'bg-blue-800 dark:bg-sky-500' : ''
+                        } absolute rounded-r-md left-0.5 -mt-2 h-10 w-2`}
+                      ></div>
                       Como Usar Swaplyar
                     </Sidebar.Item>
 
                     <Sidebar.Item
-                      className={`cursor-pointer ${
-                        selectedItem === 'loyalty-program'
-                          ? 'bg-blue-500 text-white'
-                          : 'hover:bg-gray-100 dark:hover:bg-gray-600'
-                      }`}
+                         className={`${
+                          selectedItem === 'loyalty-program'
+                            ? 'bg-gray-100 dark:bg-gray-700 h-10'
+                            : 'hover:bg-gray-100 dark:hover:bg-gray-700'
+                        }`}
                       onClick={() => handleSelect('loyalty-program')}
                       href="/info/loyalty-program"
                     >
+                       <div
+                        className={`${
+                          selectedItem === 'loyalty-program' ? 'bg-blue-800 dark:bg-sky-500' : ''
+                        } absolute rounded-r-md left-0.5 -mt-2 h-10 w-2`}
+                      ></div>
                       Programa de Fidelización
                     </Sidebar.Item>
                   </Sidebar.ItemGroup>
 
-                  <Sidebar.ItemGroup className="block md:hidden">
+                  <Sidebar.ItemGroup className="block px-2 md:hidden border-t-2 border-blue-800 dark:border-sky-500">
                     <Sidebar.Item
                       className={`flex cursor-pointer items-center gap-2 rounded-3xl border border-lightText p-3 text-center dark:border-darkText`}
                       onClick={() => {
