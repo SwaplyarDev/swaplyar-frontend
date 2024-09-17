22897648a3159ce26d596c256c4b4acd262ceafe
CarlosLagos29 <137452483+CarlosLagos29@users.noreply.github.com>
2024-09-17 09:44:05 -0300

    Segundo boton

--- components/ui/top-menu/log-register-bt/RegisterButton/registerButton.tsx
+++ components/ui/top-menu/log-register-bt/RegisterButton/registerButton.tsx
@@ -10,13 +10,20 @@ function RegisterButton() {
 
   return (
     <>
-      <Link
+      {/* <Link
         href="/auth/login-register"
         onClick={() => setView('register')}
         className={`relative m-1 hidden h-[48px] w-32 items-center justify-center rounded-3xl border border-buttonsLigth bg-buttonsLigth p-3 text-white dark:border-darkText dark:bg-darkText dark:text-lightText md:flex ${pathname === '/auth/login-register' && view === 'register' ? 'underline decoration-darkText dark:decoration-lightText' : ''} ${style.buttonLight} `}
       >
         <span className="font-bold">Registrarse</span>
         <span></span>
+      </Link> */}
+      <Link
+        href="/auth/login-register"
+        onClick={() => setView('register')}
+        className={`relative m-1 hidden h-[48px] w-32 items-center justify-center rounded-3xl border border-buttonsLigth bg-buttonsLigth p-3 text-white dark:border-darkText dark:bg-darkText dark:text-lightText md:flex ${pathname === '/auth/login-register' && view === 'register' ? 'underline decoration-darkText dark:decoration-lightText' : ''} ${style.buttonSecond} `}
+      >
+        <p className="font-bold">Registrarse</p>
       </Link>
     </>
   );
