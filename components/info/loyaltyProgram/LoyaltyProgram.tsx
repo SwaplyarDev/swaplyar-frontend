f723177cbf8377b669ad03a61f99e975eb1fd806
Ignacio Lopez <nacholopez1903@gmail.com>
2024-10-03 11:26:47 -0300

    Corrigiendo estilos

--- components/info/loyaltyProgram/LoyaltyProgram.tsx
+++ components/info/loyaltyProgram/LoyaltyProgram.tsx
@@ -19,82 +19,78 @@ function LoyaltyProgram() {
   };
   return (
     <div className="py-10">
-      <FlyerTrabajo imageSrc={CentroDeAyuda}>
-        Estamos trabajando en las funciones de inicio de sesión y registro.
-      </FlyerTrabajo>
-
-      <div className="text-center">
-        <h1 className="mb-4 text-4xl font-bold">
-          SwaplyAr Plus Rewards™ premia tu fidelidad
-        </h1>
-        <div className="mt-4">
-          <button
-            id="submit-25456"
-            className="rounded bg-blue-600 px-4 py-2 text-darkText"
-            onClick={() => handleChange()}
-          >
-            ¡Únete ya!
-          </button>
-        </div>
-      </div>
-
-      <div className="py-10">
+      <div className='pb-20'>
         <div className="text-center">
-          <h2 className="mb-4 text-2xl font-bold">
-            Obtén beneficios exclusivos cada vez que realices intercambio de
-            divisa con SwaplyAr Plus Rewards.
-          </h2>
-          <p className="">
-            ¿No estás inscrito todavía?&nbsp;
-            <Link className="text-blue-800" href="/">
-              Crea
-            </Link>
-            &nbsp;un perfil SwaplyAr o&nbsp;
-            <Link className="text-blue-800" href="/">
-              inicia
-            </Link>
-            &nbsp;sesión en tu perfil, y haz clic en &quot;inscríbite&quot; para
-            unirte.&nbsp;
-          </p>
+          <h1 className="mb-4 text-4xl font-bold">
+            SwaplyAr Plus Rewards™ premia tu fidelidad
+          </h1>
+          <div className="mt-4">
+            <button
+              id="submit-25456"
+              className="bg-[#0070ba] rounded-[23px] h-[45px] min-w-[150px] text-white border-none px-5 py-2.5 cursor-pointer hover:filter hover:brightness-95"
+              onClick={() => handleChange()}
+            >
+              ¡Únete ya!
+            </button>
+          </div>
         </div>
-      </div>
 
-      <div className="flex flex-wrap justify-center py-10">
-        {' '}
-        checkout
-        <RewardCard
-          imageSrc={Rewards1}
-          imageAlt="paso 1 de como cambiar tu dinero en SwaplyAr"
-          title="Regalo de Bienvenida"
-          description="Únete a SwaplyAr Plus Rewards™ y recibe 10 pesos por cada dólar en el cargo de tu segunda solicitud"
-          linkText="solicitud"
-          linkHref="/"
-        />
-        <RewardCard
-          imageSrc={Rewards2}
-          imageAlt="paso 2 de como cambiar tu dinero en SwaplyAr"
-          title="Los premios nunca terminan"
-          description="Recompensas Continuas: Gana 15 pesos por cada dólar en el cargo después de completar 5 solicitudes"
-          linkText="solicitudes"
-          linkHref="/"
-        />
-        <RewardCard
-          imageSrc={Rewards3}
-          imageAlt="paso 3 de como cambiar tu dinero en SwaplyAr"
-          title="Exclusivo"
-          description="¡Promociones exclusivas y acceso a ofertas únicas!"
-          linkText=""
-          linkHref=""
-        />
+        <div className="py-10">
+          <div className="text-center">
+            <h2 className="mb-4 text-2xl font-bold">
+              Obtén beneficios exclusivos cada vez que realices intercambio de
+              divisa con SwaplyAr Plus Rewards.
+            </h2>
+            <p className="">
+              ¿No estás inscrito todavía?&nbsp;
+              <Link className="text-buttonsLigth transition duration-300 ease-in-out hover:text-blue-700 dark:text-sky-500 dark:hover:text-sky-600" href="/">
+                Crea
+              </Link>
+              &nbsp;un perfil SwaplyAr o&nbsp;
+              <Link className="text-buttonsLigth transition duration-300 ease-in-out hover:text-blue-700 dark:text-sky-500 dark:hover:text-sky-600" href="/">
+                inicia
+              </Link>
+              &nbsp;sesión en tu perfil, y haz clic en &quot;inscríbite&quot;
+              para unirte.&nbsp;
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap justify-center py-10">
+          <RewardCard
+            imageSrc={Rewards1}
+            imageAlt="paso 1 de como cambiar tu dinero en SwaplyAr"
+            title="Regalo de Bienvenida"
+            description="Únete a SwaplyAr Plus Rewards™ y recibe 10 pesos por cada dólar en el cargo de tu segunda solicitud"
+            linkText="solicitud"
+            linkHref="/"
+          />
+          <RewardCard
+            imageSrc={Rewards2}
+            imageAlt="paso 2 de como cambiar tu dinero en SwaplyAr"
+            title="Los premios nunca terminan"
+            description="Recompensas Continuas: Gana 15 pesos por cada dólar en el cargo después de completar 5 solicitudes"
+            linkText="solicitudes"
+            linkHref="/"
+          />
+          <RewardCard
+            imageSrc={Rewards3}
+            imageAlt="paso 3 de como cambiar tu dinero en SwaplyAr"
+            title="Exclusivo"
+            description="¡Promociones exclusivas y acceso a ofertas únicas!"
+            linkText=""
+            linkHref=""
+          />
+        </div>
       </div>
 
       <FlyerTrabajo imageSrc={CentroDeAyuda}>
         <div>
           <p>
-            Para conocer los Términos y Condiciones del programa MoneyGram Plus
+            Para conocer los Términos y Condiciones del programa SwaplyAr Plus
             Rewards, haz
             <Link
-              className="text-blue-800"
+              className="transition duration-300 ease-in-out text-sky-500 hover:text-sky-600"
               href="/SAPR-Terms-Conditions-ES.pdf"
               target="_blank"
             >
@@ -104,7 +100,7 @@ function LoyaltyProgram() {
             .
           </p>
           <p>
-            <a className="text-blue-800" href="/info/help-center">
+            <a className="transition duration-300 ease-in-out text-sky-500 hover:text-sky-600" href="/info/help-center">
               ¿Necesitas ayuda?
             </a>
           </p>
