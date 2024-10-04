93edaefca59354dab1773a68193019e28a288495
Ludmila <lurm98@gmail.com>
2024-10-04 11:05:21 -0300

    .

--- components/info/Warranty/Warranty.tsx
+++ components/info/Warranty/Warranty.tsx
@@ -49,7 +49,7 @@ const Warranty = () => {
       <FlyerTrabajo imageSrc={CentroDeAyuda}>
         Estamos trabajando en las funciones de inicio de sesión y registro.
       </FlyerTrabajo>
-      <div className="rs-wrapper-v4 m-auto flex w-[90%] flex-col items-center justify-center gap-12">
+      <div className="rs-wrapper-v4  flex-col items-center justify-center gap-12" >
         <GuaranteeSection
           title="Garantizamos Tu Tranquilidad en Cada Transacción"
           text="En SwaplyAr, nos comprometemos a que cada cambio de divisas sea seguro y confiable. Con nuestra garantía de satisfacción, podés estar seguro de que tus operaciones se manejarán con la mayor eficiencia y cuidado. ¡Confiá en nosotros para una experiencia sin preocupaciones!"
