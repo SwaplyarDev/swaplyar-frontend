514d9666076e58795280fd4435e0329ef9788757
Ignacio Lopez <nacholopez1903@gmail.com>
2024-10-08 13:04:57 -0300

    actualizando login

--- components/auth/auth-form-login.tsx
+++ components/auth/auth-form-login.tsx
@@ -1,18 +1,12 @@
-// /auth-form-login.tsx
-
 'use client';
 
 import { useForm, SubmitHandler } from 'react-hook-form';
-import { useState } from 'react';
+import { useState, useEffect } from 'react'; // Importa useEffect
 import useStore from '@/store/authViewStore';
-import { signIn } from '@/auth';
-import { useRouter } from 'next/navigation';
-import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
-import Link from 'next/link';
-import clsx from 'clsx';
-import { useDarkTheme } from '../ui/theme-Provider/themeProvider';
 import { login } from '@/actions/auth/login';
 import Image from 'next/image';
+import { useDarkTheme } from '../ui/theme-Provider/themeProvider';
+import clsx from 'clsx';
 
 const BASE_URL = process.env.BACKEND_API_URL || 'http://localhost:8080/api/v1';
 
@@ -28,18 +22,34 @@ export const LoginForm = () => {
     formState: { errors },
     setError,
   } = useForm<FormInputs>({});
-  const { setView } = useStore();
+  const { view, setView } = useStore();
   const [loading, setLoading] = useState(false);
   const [codeSent, setCodeSent] = useState(false);
-  const [useEmail, setUseEmail] = useState<string | null>(null); 
+  const [useEmail, setUseEmail] = useState<string | null>(null);
   const [authState, setAuthState] = useState<string | null>(null);
+  const [timer, setTimer] = useState(10); // Estado para el temporizador
+  const [attempts, setAttempts] = useState(0); // Intentos de reenvío
   const { isDark } = useDarkTheme();
 
-  // Función para manejar el envío del código
+  useEffect(() => {
+    // Manejar el temporizador
+    let interval: NodeJS.Timeout | undefined;
+
+    if (timer > 0) {
+      interval = setInterval(() => {
+        setTimer((prev) => prev - 1);
+      }, 1000);
+    } else {
+      // Reiniciar intentos cuando el temporizador llegue a 0
+      setAttempts(0);
+    }
+
+    return () => clearInterval(interval); // Limpiar el intervalo al desmontar
+  }, [timer]);
+
   const sendCode = async (email: string) => {
     setLoading(true);
     try {
-      // Llamar a la API que envía el código al email
       const response = await fetch(`${BASE_URL}/login/email/send`, {
         method: 'POST',
         headers: {
@@ -48,31 +58,32 @@ export const LoginForm = () => {
         body: JSON.stringify({ email }),
       });
 
-      // if (!response.ok) {
-      //   const errorData = await response.json();
-      //   const errorMessage =
-      //     errorData?.message ||
-      //     'Error al enviar el código. Por favor, inténtalo de nuevo.';
-      //   throw new Error(errorMessage);
-      // }
-
-      setCodeSent(true); // Código enviado correctamente
-      setUseEmail(email); // Guardar el correo electrónico
+      // Remover el manejo de errores
+      setCodeSent(true);
+      setUseEmail(email);
+      setAttempts(0); // Reiniciar intentos al enviar el código
+      setTimer(10); // Reiniciar el temporizador
     } catch (error) {
       console.error('Error al enviar el código:', error);
-      alert(error || 'Ocurrió un error inesperado.');
+      alert('Ocurrió un error inesperado.');
     } finally {
       setLoading(false);
     }
   };
 
-  // Función para manejar la verificación del código
+  const resendCode = () => {
+    if (attempts < 3 && timer === 0) {
+      // Asegurarse de que el temporizador sea 0
+      sendCode(useEmail!); // Volver a enviar el código
+      setAttempts((prev) => prev + 1); // Incrementar el número de intentos
+      setTimer(10); // Reiniciar el temporizador al reenviar el código
+    }
+  };
+
   const verifyCode: SubmitHandler<FormInputs> = async ({
     verificationCode,
   }) => {
     setLoading(true);
-
-    // Validación simple del código de verificación
     if (verificationCode.length !== 6 || !/^\d+$/.test(verificationCode)) {
       setError('verificationCode', {
         type: 'manual',
@@ -83,20 +94,12 @@ export const LoginForm = () => {
     }
 
     try {
-      // Llamada a NextAuth usando 'credentials' como el método de inicio de sesión
-      // console.log(
-      //   `email: ${useEmail}`,
-      //   `verificationCode: ${verificationCode}`,
-      // );
-
       const result = await login(useEmail || '', verificationCode);
-
       if (!result.ok) {
         setAuthState('CodeInvalid');
         setError('verificationCode', {
           type: 'manual',
-          message:
-            'El código ingresado es incorrecto. Por favor, inténtalo de nuevo.',
+          message: 'El código ingresado es incorrecto.',
         });
       } else {
         setAuthState('Success');
@@ -104,15 +107,12 @@ export const LoginForm = () => {
       }
     } catch (error) {
       console.error('Error durante la verificación del código:', error);
-      alert(
-        'Ocurrió un error inesperado. Por favor, intenta de nuevo más tarde.',
-      );
+      alert('Ocurrió un error inesperado.');
     } finally {
       setLoading(false);
     }
   };
 
-  // Cambiar vista a 'register'
   const handleChange = () => {
     setView('register');
   };
@@ -129,7 +129,6 @@ export const LoginForm = () => {
           Iniciar Sesión
         </h2>
 
-        {/* Campo de correo electrónico */}
         <label
           htmlFor="email"
           className={clsx(
@@ -139,7 +138,6 @@ export const LoginForm = () => {
           Correo electrónico
         </label>
         {codeSent ? (
-          // Mostrar email como texto si el código fue enviado
           <p className="mb-5 text-gray-600 dark:text-lightText">{useEmail}</p>
         ) : (
           <input
@@ -157,14 +155,13 @@ export const LoginForm = () => {
                 message: 'El formato del correo electrónico es inválido',
               },
             })}
-            disabled={codeSent} // Deshabilitar input si el código ya fue enviado
+            disabled={codeSent}
           />
         )}
         {errors.email && (
           <p className="mb-5 text-sm text-red-500">• {errors.email.message}</p>
         )}
 
-        {/* Campo de código de verificación */}
         {codeSent && (
           <>
             <label
@@ -202,23 +199,27 @@ export const LoginForm = () => {
                 • {errors.verificationCode.message}
               </p>
             )}
+
+            {/* Botón para reenviar el código */}
+            <button
+              type="button"
+              onClick={resendCode}
+              disabled={attempts >= 3 || timer > 0} // Deshabilitar si se alcanzaron los intentos o si el temporizador no ha llegado a 0
+              className={`mt-3 h-10 rounded border border-blue-500 bg-blue-500 text-white hover:bg-blue-600 ${attempts >= 3 || timer > 0 ? 'cursor-not-allowed opacity-50' : ''}`}
+            >
+              {attempts < 3
+                ? `Reenviar código (${3 - attempts} intentos restantes)`
+                : 'Máximo de intentos alcanzado'}
+            </button>
+            {timer > 0 && (
+              <p className="text-sm text-gray-500">
+                Esperar {timer} segundos para reenviar el código.
+              </p>
+            )}
           </>
         )}
 
-        {/* Botón para enviar código o verificarlo */}
         <LoginButton pending={loading} codeSent={codeSent} />
-
-        {codeSent ? (
-          <ReLoginButton pending={loading} codeSent={codeSent} />
-        ) : (
-          ''
-        )}
-
-        <div className="my-5 flex items-center">
-          <div className="flex-1 border-t border-buttonsLigth dark:border-darkText"></div>
-          <div className="px-2 text-buttonsLigth dark:text-darkText">O</div>
-          <div className="flex-1 border-t border-buttonsLigth dark:border-darkText"></div>
-        </div>
 
         <button
           onClick={handleChange}
@@ -244,7 +245,7 @@ function LoginButton({
     <button
       type="submit"
       className={`${isDark ? 'buttonSecondDark' : 'buttonSecond'} relative m-1 h-[48px] items-center justify-center rounded-3xl border border-buttonsLigth p-3 text-buttonsLigth hover:bg-transparent dark:border-darkText dark:text-darkText dark:hover:bg-transparent`}
-      disabled={pending}
+      disabled={pending} // Desactivar el botón de verificación si está en estado de carga
     >
       {pending ? (
         <div className="flex items-center justify-center">
@@ -260,39 +261,7 @@ function LoginButton({
       ) : codeSent ? (
         'Verificar código'
       ) : (
-        'Ingresar'
-      )}
-    </button>
-  );
-}
-
-function ReLoginButton({
-  pending,
-  codeSent,
-}: {
-  pending: boolean;
-  codeSent: boolean;
-}) {
-  const { isDark } = useDarkTheme();
-  return (
-    <button
-      type="submit"
-      className={`${isDark ? 'buttonSecondDark' : 'buttonSecond'} relative m-1 h-[48px] items-center justify-center rounded-3xl border border-buttonsLigth p-3 text-buttonsLigth hover:bg-transparent dark:border-darkText dark:text-darkText dark:hover:bg-transparent`}
-      disabled={pending}
-    >
-      {pending ? (
-        <div className="flex items-center justify-center">
-          <Image
-            src="/gif/cargando.gif"
-            width={20}
-            height={20}
-            alt="loading"
-            className="mb-0.5 mr-1"
-          />
-          Procesando...
-        </div>
-      ) : (
-        codeSent && 'Reenviar codigo'
+        'Iniciar sesión'
       )}
     </button>
   );
