3db3fa5104504a4f6622ede10e75c06054188285
Nico-Paez <nico.paez099@gmail.com>
2024-10-10 13:09:08 -0300

    cambios visuales

--- components/ui/warranty-section/WarrantySection.tsx
+++ components/ui/warranty-section/WarrantySection.tsx
@@ -18,27 +18,46 @@ const GuaranteeSection: React.FC<GuaranteeSectionProps> = ({
   contentNode,
 }) => {
   return (
-    <div className="rs-wrapper-v4 flex w-full max-w-screen-lg flex-col items-center justify-center space-y-6 md:flex-row">
-      <div className="container-text md:mr-8 md:w-[65%]">
-        <h1 className="mb-2 text-pretty text-center text-3xl font-semibold md:text-left lg:text-5xl">
-          {title}
-        </h1>
-        {contentNode ? (
-          contentNode
-        ) : (
-          <p className="text-pretty text-left text-xl">{text}</p>
-        )}
+    <>
+      <div className="hidden rs-wrapper-v4 md:flex w-full max-w-screen-lg items-center justify-center space-y-6 flex-row">
+        <div className="container-text mr-8 w-[65%]">
+          <h1 className="mb-2 text-pretty text-3xl font-semibold text-left lg:text-5xl">
+            {title}
+          </h1>
+          {contentNode ? (
+            contentNode
+          ) : (
+            <p className="text-pretty text-xl">{text}</p>
+          )}
+        </div>
+        <div className="flex-shrink-0 h-auto">
+          <Image
+            className="hero-img h-96 w-auto drop-shadow-light dark:drop-shadow-darkmode"
+            src={imageSrc}
+            alt={imageAlt}
+            width={750}
+            height={750}
+          />
+        </div>
       </div>
-      <div className="h-1/2 flex-shrink-0 md:h-auto md:w-[35%]">
-        <Image
-          className="hero-img h-96 w-auto drop-shadow-light dark:drop-shadow-darkmode"
-          src={imageSrc}
-          alt={imageAlt}
-          width={750}
-          height={750}
-        />
+      <div className="md:hidden rs-wrapper-v4 flex w-full max-w-screen-lg flex-col items-center justify-center space-y-6">
+          <h1 className="mb-2 text-pretty text-center text-3xl font-semibold lg:text-5xl">
+            {title}
+          </h1>
+          <Image
+            className="hero-img h-96 w-auto drop-shadow-light dark:drop-shadow-darkmode"
+            src={imageSrc}
+            alt={imageAlt}
+            width={750}
+            height={750}
+          />
+          {contentNode ? (
+            contentNode
+          ) : (
+            <p className="text-pretty text-xl">{text}</p>
+          )}
       </div>
-    </div>
+    </>
   );
 };
 
