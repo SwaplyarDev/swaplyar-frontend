020adcd6d96566f3405858309498aa39ceb4453a
alan damian Fernandez <ceo_oa@swaplyar.com>
2024-10-10 21:42:49 -0300

    fix - action.requestRegister.tsx

--- actions/request/action.requestRegister.tsx
+++ actions/request/action.requestRegister.tsx
@@ -2,45 +2,7 @@
 
 'use server';
 
-type TransactionRequest = {
-  transaction: {
-    sender: {
-      first_name: string;
-      last_name: string;
-      identification: string;
-      phone_number: string;
-      email: string;
-      bank_account: {
-        email_account: string;
-        payment_method: string;
-        number_account: string;
-      };
-    };
-    receiver: {
-      first_name: string;
-      last_name: string;
-      bank_account: {
-        email_account: string;
-        payment_method: string;
-        number_account: string;
-      };
-    };
-    transfer: {
-      transfer_code: string;
-      country_transaction: string;
-      message: string;
-    };
-    amounts: {
-      amount_sent: number;
-      currency_sent: string;
-      amount_received: number;
-      currency_received: string;
-    };
-    proof_of_payment: {
-      img_transaction: string; // base64 string o URL
-    };
-  };
-};
+import { TransactionRequest } from "@/types/request/request";
 
 
 export const requestRegister = async (transaction: TransactionRequest) => {
