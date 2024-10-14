fb8348facc810324527e412e54e634babd4e9d0d
Germann4 <germandiorio8@gmail.com>
2024-10-13 22:10:53 -0300

    Reemplacé la etiqueta <a> por Link y eliminé el atributo style inline.

--- components/ui/LinkWithHover/LinkWithHover.tsx
+++ components/ui/LinkWithHover/LinkWithHover.tsx
@@ -20,7 +20,6 @@ const LinkWithHover: FC<LinkWithHoverProps> = ({ href, children }) => {
     <a
       href={href}
       target="_blank"
-      style={{ color: '#012a8d', textDecoration: 'none' }}
       onMouseOver={handleMouseOver}
       onMouseOut={handleMouseOut}
       className="relative m-1 flex h-[48px] items-center gap-2 transition-transform duration-300 ease-in-out hover:scale-105 hover:text-blue-700 dark:text-sky-500 dark:hover:text-sky-600"
