c0ab9c9a97ba922cffb5a315756f131476fe3377
Germann4 <germandiorio8@gmail.com>
2024-10-17 22:30:21 -0300

    Corrige el estilo del enlace en el componente

--- components/ui/LinkWithHover/LinkWithHover.tsx
+++ components/ui/LinkWithHover/LinkWithHover.tsx
@@ -22,7 +22,7 @@ const LinkWithHover: FC<LinkWithHoverProps> = ({ href, children }) => {
       target="_blank"
       onMouseOver={handleMouseOver}
       onMouseOut={handleMouseOut}
-      className="relative m-1 flex h-[48px] items-center gap-2 transition-transform duration-300 ease-in-out hover:scale-105 hover:text-blue-700 dark:text-sky-500 dark:hover:text-sky-600"
+      className="relative m-1 inline-flex h-[48px] items-center gap-2 transition-transform duration-300 ease-in-out hover:scale-105 hover:text-blue-700 dark:text-sky-500 dark:hover:text-sky-600"
     >
       {children}
     </a>
