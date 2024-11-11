9576475a6ee165520a5901c68c98e33148e1c285
Germann4 <germandiorio8@gmail.com>
2024-11-11 02:57:50 -0300

    Agregue scroll al top en paginación y botones de navegación al carrusel.

--- components/info/blog/Blog.tsx
+++ components/info/blog/Blog.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 // @ts-ignore
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
@@ -36,31 +36,71 @@ const BlogPostCard: React.FC<{ title: string; body: string; url_image: string; c
 
 const ImageCarousel: React.FC<{ images: string[] }> = ({ images }) => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const sliderRef = useRef<Slider>(null);
 
   const settings = {
     infinite: true,
-    speed: 500,
+    speed: 500, // velocidad de transición
     slidesToShow: 1,
     slidesToScroll: 1,
     beforeChange: (current: number, next: number) => setActiveIndex(next),
     autoplay: true,
     autoplaySpeed: 5000,
+    arrows: false, // Oculta los botones de flechas predeterminados
+    ref: sliderRef, // Para controlar el slider manualmente
   };
 
   const limitedImages = images.slice(0, 6);
 
+  // Funciones para navegar y reiniciar autoplay
+  const goToPreviousSlide = () => {
+    sliderRef.current?.slickPrev();
+  };
+
+  const goToNextSlide = () => {
+    sliderRef.current?.slickNext();
+  };
+
   return (
-    <div className="border-4 border-[#012C8A] rounded-[20px] overflow-hidden relative">
+    <div className="relative group border-4 border-[#012C8A] rounded-[20px] overflow-hidden">
       <Slider {...settings}>
         {limitedImages.map((image, index) => (
           <div key={index}>
-            <Image src={image} alt={`Slide ${index + 1}`} className="w-full h-60 object-cover" width={800} height={400} />
+            <Image
+              src={image}
+              alt={`Slide ${index + 1}`}
+              className="w-full h-60 object-cover"
+              width={800}
+              height={400}
+            />
           </div>
         ))}
       </Slider>
+
+      {/* Botón de navegación izquierda */}
+      <button
+        className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-gray-700 bg-opacity-50 text-white rounded-full p-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+        onClick={goToPreviousSlide}
+      >
+        &lt;
+      </button>
+
+      {/* Botón de navegación derecha */}
+      <button
+        className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-gray-700 bg-opacity-50 text-white rounded-full p-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+        onClick={goToNextSlide}
+      >
+        &gt;
+      </button>
+
       <div className="absolute bottom-4 w-full flex justify-center space-x-2">
         {limitedImages.map((_, index) => (
-          <div key={index} className={`w-4 h-4 rounded-full transition-all duration-300 ${activeIndex === index ? 'bg-[#012C8A]' : 'bg-white border-2 border-[#012C8A] opacity-50'}`}></div>
+          <div
+            key={index}
+            className={`w-4 h-4 rounded-full transition-all duration-300 ${
+              activeIndex === index ? "bg-[#012C8A]" : "bg-white border-2 border-[#012C8A] opacity-50"
+            }`}
+          ></div>
         ))}
       </div>
     </div>
@@ -91,6 +131,12 @@ const Blog: React.FC = () => {
 
   const handlePageChange = (page: number) => {
     if (page !== currentPage) {
+
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
       setCurrentPage(page);
       router.replace(`/info/blog?page=${page}`, { scroll: false });
     }
