bf936979c3c7ebc2c8698f18cd38becc5fe26e73
alan damian Fernandez <92124271+Alan-Fernandez@users.noreply.github.com>
2024-11-21 17:24:05 -0300

    feat(blog): wrap Blog component in Suspense for useSearchParams

--- app/info/blog/page.tsx
+++ app/info/blog/page.tsx
@@ -1,10 +1,11 @@
+import React, { Suspense } from 'react';
 import Blog from '@/components/info/blog/Blog';
 
 const BlogPage: React.FC = () => {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Blog />
-    </>
+    </Suspense>
   );
 };
 
