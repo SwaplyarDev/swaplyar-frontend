7996f8776d1d4a00a2277026280a5f78cd995d7a
Nico-Paez <nico.paez099@gmail.com>
2024-12-12 13:05:28 -0300

    continuo con cambios visuales del apartado de preguntas frecuentes

--- components/info/frequentlyQuestions/FrequentlyQuestions.tsx
+++ components/info/frequentlyQuestions/FrequentlyQuestions.tsx
@@ -2,22 +2,23 @@
 
 import React from 'react';
 import { styled } from '@mui/material/styles';
-import { ExpandMore } from '@mui/icons-material'; // Importamos el nuevo icono de expansión
+import { ExpandMore } from '@mui/icons-material';
 import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
 import MuiAccordionSummary, { AccordionSummaryProps, accordionSummaryClasses } from '@mui/material/AccordionSummary';
 import MuiAccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
+import { useDarkTheme } from '@/components/ui/theme-Provider/themeProvider';
 
-const Accordion = styled((props: AccordionProps) => <MuiAccordion disableGutters elevation={0} square {...props} />)(
-  ({ theme }) => ({
-    border: `1px solid ${theme.palette.divider}`,
-    '&:not(:last-child)': {
-      borderBottom: 0,
+// Estilo del Accordion con uso de la prop `isDark`
+const Accordion = styled((props: AccordionProps & { isDark: boolean }) => <MuiAccordion disableGutters elevation={0} square {...props} />)(
+  ({ isDark }) => ({
+    border: 'none', // Asegura que no haya bordes extra
+    borderTop: isDark ? '1px solid #ebe7e0' : '1px solid #252526', // Borde superior según el tema
+    backgroundColor: 'transparent',  // Aseguramos que el fondo sea transparente
+    '&:last-child': {
+      borderBottom: isDark ? '1px solid #ebe7e0' : '1px solid #252526', // Borde inferior según el tema
     },
-    '&::before': {
-      display: 'none',
-    },
-  }),
+  })
 );
 
 const AccordionSummary = styled((props: AccordionSummaryProps) => (
@@ -28,86 +29,79 @@ const AccordionSummary = styled((props: AccordionSummaryProps) => (
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
-          borderRadius: '50%', // Esto crea el círculo
-          width: '30px', // Tamaño del círculo
-          height: '30px', // Tamaño del círculo
-          backgroundColor: '#ddd', // Color de fondo del círculo
+          borderRadius: '50%',
+          width: '40px',
+          height: '40px',
+          backgroundColor: '#ddd',
         }}
       >
-        <ExpandMore sx={{ fontSize: '1.5rem', color: '#333' }} /> {/* Aquí ajustas el icono */}
+        <ExpandMore sx={{ fontSize: '2rem', color: '#333' }} />
       </div>
     }
     {...props}
   />
 ))(({ theme }) => ({
-  backgroundColor: 'rgba(0, 0, 0, .03)',
-  flexDirection: 'row', // Cambié el flexDirection para que el icono esté a la derecha
+  border: 'none', // Eliminar cualquier borde en el summary
   [`& .${accordionSummaryClasses.expandIconWrapper}.${accordionSummaryClasses.expanded}`]: {
-    transform: 'rotate(180deg)', // Rota el icono cuando el panel está expandido
+    transform: 'rotate(180deg)',
   },
   [`& .${accordionSummaryClasses.content}`]: {
-    marginLeft: theme.spacing(1),
+    marginLeft: theme.spacing(0),
   },
-  ...theme.applyStyles('dark', {
-    backgroundColor: 'rgba(255, 255, 255, .05)',
-  }),
 }));
 
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
   padding: theme.spacing(2),
-  borderTop: '1px solid rgba(0, 0, 0, .125)',
 }));
 
 const FrequentlyQuestions = () => {
-  const [expanded, setExpanded] = React.useState<Set<string>>(new Set()); // Usamos un Set para almacenar los paneles abiertos
+  const [expanded, setExpanded] = React.useState<Set<string>>(new Set());
+  const { isDark } = useDarkTheme();
 
   const handleChange = (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
     setExpanded((prevExpanded) => {
       const newExpandedSet = new Set(prevExpanded);
       if (newExpanded) {
-        newExpandedSet.add(panel); // Si se abre, agregamos el panel al Set
+        newExpandedSet.add(panel);
       } else {
-        newExpandedSet.delete(panel); // Si se cierra, eliminamos el panel del Set
+        newExpandedSet.delete(panel);
       }
       return newExpandedSet;
     });
   };
 
   return (
-    <div>
-      <Accordion expanded={expanded.has('panel1')} onChange={handleChange('panel1')}>
+    <div className='m-14'>
+      <Accordion expanded={expanded.has('panel1')} onChange={handleChange('panel1')} isDark={isDark}>
         <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-          <Typography>Collapsible Group Item #1</Typography>
+          <Typography className={`${isDark ? 'text-[#ebe7e0]' : 'text-[#252526]'}`}>Collapsible Group Item #1</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit
-            leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
+        <AccordionDetails className='flex justify-center'>
+          <Typography className='bg-gray-200 p-6 rounded-md text-left w-9/10'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sitasdda amet blandit
+            leo lobortis eget.
           </Typography>
         </AccordionDetails>
       </Accordion>
-      <Accordion expanded={expanded.has('panel2')} onChange={handleChange('panel2')}>
+      <Accordion expanded={expanded.has('panel2')} onChange={handleChange('panel2')} isDark={isDark}>
         <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
-          <Typography>Collapsible Group Item #2</Typography>
+          <Typography className={`${isDark ? 'text-[#ebe7e0]' : 'text-[#252526]'}`}>Collapsible Group Item #2</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
+        <AccordionDetails className='flex justify-center'>
+          <Typography className='bg-gray-200 p-6 rounded-md text-left w-9/10'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit
-            leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
+            leo lobortis eget.
           </Typography>
         </AccordionDetails>
       </Accordion>
-      <Accordion expanded={expanded.has('panel3')} onChange={handleChange('panel3')}>
+      <Accordion expanded={expanded.has('panel3')} onChange={handleChange('panel3')} isDark={isDark}>
         <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
-          <Typography>Collapsible Group Item #3</Typography>
+          <Typography className={`${isDark ? 'text-[#ebe7e0]' : 'text-[#252526]'}`}>Collapsible Group Item #3</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
+        <AccordionDetails className='flex justify-center'>
+          <Typography className='bg-gray-200 p-6 rounded-md text-left w-9/10'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit
-            leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
+            leo lobortis eget.
           </Typography>
         </AccordionDetails>
       </Accordion>
