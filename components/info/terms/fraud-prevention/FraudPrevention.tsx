cd4ebe41e02524c7e009c74d87908d7b81c9886a
Marcos Ramirez <Gellollo@outlook.com>
2025-01-16 13:05:48 -0300

    fix: arreglos a los estilos ya que no cumplian los requisitos del figma

--- components/info/terms/fraud-prevention/FraudPrevention.tsx
+++ components/info/terms/fraud-prevention/FraudPrevention.tsx
@@ -1,5 +1,7 @@
 'use client';
 import FlyerTrabajo from '@/components/FlyerTrabajo/FlyerTrabajo';
+import { useMargins } from '@/context/MarginProvider';
+import { ResponsiveMarginHook } from '@/hooks/ResponsiveMarginHook';
 import { fraudPrevention } from '@/utils/assets/img-database';
 import { mockLinks } from './mockFraudPrev';
 import { mockTexts } from './mockFraudPrev';
@@ -8,6 +10,8 @@ import { useState } from 'react';
 const FraudPrevention = () => {
   const [selectedTextIndex, setSelectedTextIndex] = useState<number>(0);
   const [isAnimating, setIsAnimating] = useState(false);
+  const { margins } = useMargins();
+  const currentMargin = ResponsiveMarginHook(margins);
 
   const handleButtonClick = (key: number) => {
     setIsAnimating(true);
@@ -32,49 +36,51 @@ const FraudPrevention = () => {
   return (
     <main className="flex w-full flex-col items-center gap-10 pt-10">
       <h1 className="self-center text-2xl font-medium md:text-3xl lg:text-4xl">Concientización sobre el fraude</h1>
-      <section className="flex min-h-[100vh] w-[80%] flex-col justify-items-center gap-5 lg:grid lg:grid-cols-2">
-        <article className="flex select-none flex-col items-start gap-5 pt-5 text-xl lg:text-2xl">
-          {renderButtons(0, 'Prevencion del fraude')}
-          {renderButtons(1, 'Como protegerse del fraude')}
-          {renderButtons(2, 'Estafas al cliente comunes')}
-          {renderButtons(3, 'Otros recursos')}
-        </article>
-        <article className="flex flex-col gap-5">
-          <h2 className="border-t-[1px] border-blue-500 text-xl font-semibold dark:border-[#EBE7E0] md:text-2xl lg:text-3xl">
-            {mockTexts[selectedTextIndex]?.title}
-          </h2>
-          {selectedTextIndex !== null ? (
-            <section
-              className={`transition-all duration-300 ease-in-out ${
-                isAnimating ? 'max-h-0 opacity-0' : 'max-h-[100%] opacity-100'
-              } flex flex-col gap-5 overflow-hidden rounded-md bg-[#EEEAE3] p-5 dark:bg-[#4B4B4B]`}
-            >
-              <article className="flex flex-col gap-2">
-                <p>{mockTexts[selectedTextIndex]?.text1}</p>
-                <p>{mockTexts[selectedTextIndex]?.text2}</p>
-              </article>
-              <article className="flex flex-col gap-1">
-                {selectedTextIndex === 2
-                  ? mockLinks.map((mock, index) => (
-                      <a
-                        key={index}
-                        className="text-blue-600 underline decoration-blue-600 transition-all duration-150 hover:text-blue-800 hover:decoration-blue-800 dark:text-[#EBE7E0] dark:decoration-[#EBE7E0] dark:hover:decoration-[#97938d]"
-                        href={mock.link}
-                      >
-                        {mock.text}
-                      </a>
-                    ))
-                  : null}
-              </article>
+      <section style={{ margin: currentMargin }}>
+        <section className="flex min-h-[100vh] max-w-[1050px] flex-col justify-items-start gap-20 lg:flex-row">
+          <article className="flex select-none flex-col items-start gap-4 pt-5 text-xl lg:text-2xl">
+            {renderButtons(0, 'Prevencion del fraude')}
+            {renderButtons(1, 'Como protegerse del fraude')}
+            {renderButtons(2, 'Estafas al cliente comunes')}
+            {renderButtons(3, 'Otros recursos')}
+          </article>
+          <article className="flex flex-col gap-5">
+            <h2 className="absolute justify-self-end border-blue-500 p-2 text-xl font-semibold dark:border-[#EBE7E0] md:text-2xl lg:border-t-[1px] lg:text-3xl">
+              {mockTexts[selectedTextIndex]?.title}
+            </h2>
+            {selectedTextIndex !== null ? (
+              <section
+                className={`mt-12 shrink-0 self-end transition-all duration-300 ease-in-out md:max-w-[20rem] lg:max-w-[30rem] ${
+                  isAnimating ? 'max-h-0 opacity-0' : 'max-h-[100%] opacity-100'
+                } flex flex-col gap-5 overflow-hidden rounded-md bg-[#EEEAE3] p-5 text-sm dark:bg-[#4B4B4B] lg:text-base`}
+              >
+                <article className="flex flex-col gap-2">
+                  <p>{mockTexts[selectedTextIndex]?.text1}</p>
+                  <p>{mockTexts[selectedTextIndex]?.text2}</p>
+                </article>
+                <article className="flex flex-col gap-1">
+                  {selectedTextIndex === 2
+                    ? mockLinks.map((mock, index) => (
+                        <a
+                          key={index}
+                          className="text-blue-600 underline decoration-blue-600 transition-all duration-150 hover:text-blue-800 hover:decoration-blue-800 dark:text-[#EBE7E0] dark:decoration-[#EBE7E0] dark:hover:decoration-[#97938d]"
+                          href={mock.link}
+                        >
+                          {mock.text}
+                        </a>
+                      ))
+                    : null}
+                </article>
 
-              <p>{mockTexts[selectedTextIndex]?.text3}</p>
-            </section>
-          ) : (
-            <p>Selecciona una opción para ver más detalles.</p>
-          )}
-        </article>
+                <p>{mockTexts[selectedTextIndex]?.text3}</p>
+              </section>
+            ) : (
+              <p>Selecciona una opción para ver más detalles.</p>
+            )}
+          </article>
+        </section>
       </section>
-      <div className="relative top-20 mt-16 hidden pt-11 lg:contents">
+      <div className="my-20 flex w-[100%] flex-row">
         <FlyerTrabajo imageSrc={fraudPrevention}>
           <> </>
         </FlyerTrabajo>
