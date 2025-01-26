4e892859fa34ab5b498787bfbc2604ec50412190
felilozi <felicialozinski97@gmail.com>
2025-01-26 16:26:49 -0300

    feat: error 500 actualizacion del tamaño de las imagenes

--- components/ErrorBoundary/errorBoundary.tsx
+++ components/ErrorBoundary/errorBoundary.tsx
@@ -29,57 +29,53 @@ const ErrorBoundary = () => {
   return (
     <>
       <section className="relative m-auto flex w-full max-w-7xl flex-col items-start gap-20">
-        <section className="relative flex min-h-[490px] w-full items-end xs:min-h-[530px] xs-phone:min-h-[550px] md-tablet:min-h-[630px]">
-          <article className="absolute top-0 flex w-full items-center justify-center">
+        <section className="relative flex min-h-[480px] w-full items-end xs-mini-phone:min-h-[540px] xs-mini-phone2:h-[544px] xs:h-[544px] md-phone:min-h-[475px] sm-tablet:min-h-[550px]">
+          <article className="absolute top-0 flex w-full items-center justify-center bg-blue-300">
             <Image
               src={isDark ? Error_500_dark : Error_500_clear}
-              className="hidden px-8 md-phone:block xl-desktop:px-0 xl:w-[700px]"
+              className="hidden w-[400px] px-8 lg:block xl-desktop:px-0"
               alt="Descripción de la imagen 500"
               width={987}
               height={578}
             />
             <Image
               src={isDark ? Error_500_dark : Error_500_clear}
-              className="block px-6 xs:px-10 sm-phone:px-8 md-phone:hidden xl-desktop:px-0"
+              className="block xs-mini-phone2:min-w-[400px] xs-mini-phone2:max-w-[426px] xs-phone:max-w-[500px] xs-phone:bg-red-500 lg:hidden"
               alt="Descripción de la imagen 500"
               width={600}
               height={400}
-              style={{
-                background: 'green',
-              }}
             />
           </article>
-          {/* Contenido principal */}
-          <article className="ml-2 flex w-full flex-col gap-3 border-l-2 border-l-buttonsLigth pl-1 dark:border-l-darkText xs-mini-phone:ml-6 xs:pl-6 md-phone:min-w-[350px] sm-tablet:min-w-[440px] md-tablet:ml-12">
-            <h1 className="hidden text-start text-5xl sm-phone:block">
+          {/* Texto------------------------------ */}
+          <article className="z-20 ml-2 flex min-h-[200px] flex-col justify-center border-l-2 border-l-buttonsLigth pt-2 dark:border-l-darkText xs-mini-phone2:ml-3 xs-mini-phone2:min-w-[380px] xs:ml-12 xs-phone:ml-6 xs-phone:pt-5 sm-tablet:ml-8">
+            <h1 className="titleFont ml-3 hidden text-start text-5xl font-bold md-phone:block">
+              Ups...
+              <br />
+              Algo salió mal
+              <br />
+              en nuestro lado
+            </h1>
+            <h1 className="titleFont xs-phone: ml-7 block text-start text-4xl font-bold xs-mini-phone:text-5xl xs-phone:text-4xl md-phone:hidden">
               Ups...
               <br />
               Algo salió mal
               <br />
               en nuestro lado.
             </h1>
-            <h1 className="block text-start text-3xl mini-phone:text-4xl sm-phone:hidden md-phone:text-5xl">
-              Ups...
-              <br />
-              La página no
-              <br />
-              ha sido
-              <br />
-              encontrada
-            </h1>
 
             {/* Botón */}
             <Link href="/">
-              <button className="flex w-[165px] items-center justify-center rounded-full border border-buttonsLigth bg-transparent p-2 text-base font-bold text-buttonsLigth dark:border-darkText dark:text-darkText">
+              <button className="ml-3 flex w-[165px] justify-center rounded-full border border-buttonsLigth bg-transparent p-2 text-base font-bold text-buttonsLigth dark:border-darkText dark:text-darkText">
                 Redireccion en {counter}
               </button>
             </Link>
           </article>
-          <article className="absolute flex min-h-[530px] w-full items-end justify-end pb-10 pr-2 mini-phone:mb-44 xs:pb-0 xs:pr-0 md-phone:items-center md:relative md:justify-center md:pr-14 md-tablet:min-h-[630px] lg:mb-0 lg:pb-0 lg:pr-0 xl-desktop:items-end xl:mb-0">
+          {/* Imagen dos ........................ */}
+          <article className="xs absolute top-32 flex w-full items-center justify-center xs-mini-phone:top-36 md-phone:right-4 md-phone:top-44 md-phone:items-end md-phone:justify-end sm-tablet:top-60 md-tablet:min-h-[630px]">
             <Image
               src={Post1_500}
               alt="Woman smiling with tablet"
-              className="z-10 w-[310px] object-cover xs:w-[358px] md-tablet:w-[425px] xl-desktop:w-[800px]"
+              className="z-10 xs-mini-phone2:min-w-[400px] xs-mini-phone2:max-w-[426px]"
               width={800}
               height={437}
             />
