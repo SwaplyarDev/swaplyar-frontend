ee0358adb01723dff53ec8882a39d0f627cd9311
Marcos Ramirez <Gellollo@outlook.com>
2025-01-27 11:40:20 -0300

    fix: arreglo de estilos fraud prevention

--- components/info/terms/fraud-prevention/mobile/FraudMobile.tsx
+++ components/info/terms/fraud-prevention/mobile/FraudMobile.tsx
@@ -8,14 +8,23 @@ interface FraudMobileProps {
 
 const FraudMobile: React.FC<FraudMobileProps> = ({ selectedTextIndex, handleButtonClick, isAnimating }) =>
   mockTexts.map((item, key) => (
-    <section key={key} className="flex w-[100%] flex-col items-start gap-2 lg:hidden">
+    <section
+      key={key}
+      id={`section-${key}`} // ID único para cada sección
+      className="flex w-[100%] flex-col items-start gap-2 lg:hidden"
+    >
       {/* Botón */}
       <button
-        className={`flex select-none flex-row gap-2 text-xl ${
+        className={`flex select-none flex-row gap-2 text-xl font-extrabold ${
           selectedTextIndex === key
             ? 'scale-105 text-blue-800 decoration-blue-800 dark:text-[#EBE7E0] dark:decoration-[#EBE7E0]'
             : 'dark:text-[#EBE7E0] dark:decoration-[#EBE7E0]'
         } transition-all duration-150 hover:text-blue-800 hover:decoration-blue-800 dark:hover:decoration-[#EBE7E0]`}
+        onClick={() => {
+          handleButtonClick(key); // Manejador de evento actual
+          const section = document.getElementById(`section-${key}`);
+          !isAnimating ? section?.scrollIntoView({ behavior: 'smooth', block: 'start' }) : null; // Desplazamiento suave
+        }}
       >
         <h3>{selectedTextIndex === key ? '|  ' : null}</h3>
         <h3 className={`${selectedTextIndex === key ? 'underline' : 'decoration-transparent'}`}>{item.button}</h3>
