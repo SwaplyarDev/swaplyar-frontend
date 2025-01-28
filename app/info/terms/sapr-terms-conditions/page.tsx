d003cb2ad57f194a3873a94ccd8eb066da5dfb6d
Marcos Ramirez <Gellollo@outlook.com>
2025-01-28 09:11:06 -0300

    fix: arreglo en el error de seoComp

--- app/info/terms/sapr-terms-conditions/page.tsx
+++ app/info/terms/sapr-terms-conditions/page.tsx
@@ -1,11 +1,11 @@
 import SaprTermsConditions from '@/components/info/terms/sapr-terms-conditions/SaprTermsConditions';
-import { constructMetadata } from '@/components/seo/SeoComp';
+import { Metadata } from 'next';
 
-export const metadata = constructMetadata({
+export const metadata: Metadata = {
   title: 'Términos y Condiciones del Programa de Fidelización SwaplyAr',
   description:
     'Consulta los Términos y Condiciones de SwaplyAr Plus Rewards - Programa de Fidelización. Descubre cómo disfrutar de tus beneficios con total seguridad.',
-});
+};
 
 const page = () => {
   return (
