b036934e9aa0b91a93434a95a2be5a3f41107490
anatmb <anatmb26@hotmail.com>
2025-01-28 02:46:35 -0300

    feat(maintenance): correcion de estilo

--- components/maintenance/maintenance.tsx
+++ components/maintenance/maintenance.tsx
@@ -25,31 +25,31 @@ const MaintenancePage = ({ params }: MaintenanceProps) => {
     }
 
     const timer = setInterval(() => {
-      setCounter((prev) => prev - 1);
+      // setCounter((prev) => prev - 1);
     }, 1000);
 
     return () => clearInterval(timer);
   }, [counter, router]);
 
   return (
-    <section className="m-auto flex w-full max-w-7xl flex-col items-start pb-24 sm:flex-row-reverse">
+    <section className="m-auto flex max-h-[600px] w-full flex-col items-start px-3 xs:max-h-[900px] xs-phone:max-h-[1000px] sm:flex-row-reverse lg:max-h-[800px] lg:max-w-7xl">
       {/* Contenedor del texto */}
-      <section className="sm:640px md-phone:700px flex w-full flex-col px-5 py-11 sm:pr-[100px] sm:pt-[60px] md:pr-[110px] md:pt-[60px] lg:pr-[300px] lg:pt-[80px]">
+      <section className="flex w-full flex-col">
         {/* Títulos */}
-        <h1 className="hidden text-right sm:block sm:text-[28px] md:text-[35px] lg:text-[40px]">
+        <h1 className="hidden text-right font-titleFont font-bold sm:block sm:pr-4 sm:pt-4 sm:text-[30px] md:text-[38px] lg:pr-[185px] lg:pt-[90px] lg:text-[33px]">
           Sitio Web
           <br />
           en Mantenimiento
         </h1>
 
-        <h1 className="block text-right font-['Inter'] text-[38px] font-medium leading-[45.60px] sm:hidden md-phone:text-5xl">
+        <h1 className="block text-right font-titleFont text-[29px] font-bold leading-[1.2] xs-mini-phone:text-[35px] xs:text-[40px] xs-phone:text-[45px] sm:hidden">
           Sitio Web
           <br />
           en Mantenimiento
         </h1>
 
-        {/* Parrafo que aparece en pantallas grandes */}
-        <p className="hidden text-right text-[20px] sm:block sm:text-[18px] md:text-[25px] lg:text-[30px]">
+        {/* Párrafo para pantallas grandes */}
+        <p className="lg:pt-[296px ] hidden text-right font-textFont text-[28px] sm:block sm:pr-4 sm:text-[25px] md:text-[30px] lg:pr-[185px] lg:text-[30px]">
           Disculpe las molestias
           <br />
           estamos trabajando
@@ -58,31 +58,29 @@ const MaintenancePage = ({ params }: MaintenanceProps) => {
         </p>
 
         {/* Sección en mantenimiento */}
-        <div>
-          {section && (
-            <p className="text-right text-lg font-semibold">
-              La sección <span className="text-blue-500 dark:text-blue-300">{section}</span> está en mantenimiento.
-            </p>
-          )}
-        </div>
+        {section && (
+          <p className="text-right text-lg font-semibold sm:pr-4 lg:pr-[160px]">
+            La sección <span className="text-blue-500 dark:text-blue-300">{section}</span> está en mantenimiento.
+          </p>
+        )}
 
         {/* Redirección */}
-        <div className="hidden w-full text-right sm:block">
+        <div className="hidden w-full text-right sm:block sm:pr-4 lg:pr-[185px]">
           <Link href="/">
             <div className="inline-flex w-[165px] items-center justify-center rounded-full border border-buttonsLigth bg-transparent p-2 text-base font-bold text-buttonsLigth dark:border-darkText dark:text-darkText">
-              Redireccion en {counter}
+              Redirección en {counter}
             </div>
           </Link>
         </div>
       </section>
 
       {/* Contenedor de la imagen */}
-      <section className="relative flex min-h-[400px] w-full items-end xs:min-h-[400px] xs-phone:min-h-[400px] md-tablet:min-h-[740px]">
-        <article className="absolute top-0 flex w-full flex-col items-center justify-center sm:left-[118px] sm:top-[104px] sm:z-50 md:left-[118px] md:top-[154px] lg:left-[285px] lg:top-[204px]">
+      <section className="relative flex min-h-[600px] w-full items-end xs:min-h-[650px] md:min-h-[600px] lg:min-h-[740px]">
+        <article className="absolute top-0 flex w-full flex-col items-center justify-center sm:left-[80px] sm:top-[80px] md:left-[100px] md:top-[120px] lg:left-[180px] lg:top-[160px]">
           {/* Imagen para pantallas grandes */}
           <Image
             src={Mantenimiento}
-            className="hidden sm:block sm:h-[395.77px] sm:w-[569px] xl-desktop:px-0"
+            className="hidden sm:block sm:max-w-[500px] md:max-w-[600px] lg:max-w-[750px]"
             alt="Descripción de la imagen mantenimiento"
             width={1000}
             height={400}
@@ -91,15 +89,15 @@ const MaintenancePage = ({ params }: MaintenanceProps) => {
           {/* Imagen para pantallas pequeñas */}
           <Image
             src={Mantenimiento}
-            className="block px-6 xs:px-10 sm:hidden xl-desktop:px-0"
+            className="block px-4 sm:hidden"
             alt="Descripción de la imagen mantenimiento"
             width={600}
             height={400}
           />
 
-          {/* Parrafo que aparece en pantallas pequeñas */}
+          {/* Párrafo para pantallas pequeñas */}
 
-          <p className="mb-5 mr-9 block w-full text-right text-[25px] sm:hidden">
+          <p className="mb-5 mt-4 w-full text-right font-textFont text-[25px] xs-mini-phone:text-[30px] xs:text-[35px] xs-phone:text-[43px] sm:hidden">
             Disculpe las molestias
             <br />
             estamos trabajando
@@ -107,10 +105,11 @@ const MaintenancePage = ({ params }: MaintenanceProps) => {
             para mejorar el servicio.
           </p>
 
-          <div className="mr-9 block w-full text-right sm:hidden">
+          {/* Botón de redirección para pantallas pequeñas */}
+          <div className="w-full text-right sm:hidden">
             <Link href="/">
               <div className="inline-flex w-[165px] items-center justify-center rounded-full border border-buttonsLigth bg-transparent p-2 text-base font-bold text-buttonsLigth dark:border-darkText dark:text-darkText">
-                Redireccion en {counter}
+                Redirección en {counter}
               </div>
             </Link>
           </div>
