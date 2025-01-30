f456af14c2f1f6ca519876633065fe4ffb990b50
felilozi <felicialozinski97@gmail.com>
2025-01-30 12:31:34 -0300

    fix: mejoras en responsivo, márgenes y finalización de página

--- components/maintenance/maintenance.tsx
+++ components/maintenance/maintenance.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation'; // Usa next/navigation en lugar de 
 import Image from 'next/image';
 import Link from 'next/link';
 import { Mantenimiento } from '@/utils/assets/img-database';
+import RedirectButton from '../ui/RedirectButton/RedirectButton';
 
 interface MaintenanceProps {
   params?: {
@@ -25,94 +26,47 @@ const MaintenancePage = ({ params }: MaintenanceProps) => {
     }
 
     const timer = setInterval(() => {
-      // setCounter((prev) => prev - 1);
+      setCounter((prev) => prev - 1);
     }, 1000);
 
     return () => clearInterval(timer);
   }, [counter, router]);
 
   return (
-    <section className="m-auto flex min-h-[450px] w-full flex-col items-start px-3 xs-mini-phone:min-h-[500px] xs-mini-phone2:min-h-[570px] xs:max-h-[623px] xs-phone:max-h-[1000px] sm:flex-row-reverse lg:max-h-[800px] lg:max-w-7xl">
+    <section className="relative m-auto flex w-full max-w-7xl items-center gap-20">
       {/* Contenedor del texto */}
-      <section className="flex w-full flex-col">
-        {/* Títulos */}
-        <h1 className="titleFont hidden bg-red-200 text-right sm:block sm:pr-4 sm:pt-4 sm:text-[30px] md:text-[38px] lg:pr-[185px] lg:pt-[90px] lg:text-[33px]">
-          Sitio Web
-          <br />
-          en Mantenimiento
-        </h1>
+      <section className="flex w-full flex-col items-end px-4 xs-mini-phone2:px-5 xs-phone:px-9 md-phone:h-[570px] md-phone:px-1 sm-tablet:pr-[50px] md:pr-[50px] lg-tablet:h-[700px]">
+        <article className="md-phone:flex-row-reverse lg-tablet:absolute lg-tablet:right-[135px] lg-tablet:top-[49px] lg2:right-[212px]">
+          {/* Título único */}
+          <h1 className="titleFont text-right text-[33px] xs-mini-phone:text-[36px] xs-mini-phone2:text-[37px] xs:text-[43px] xs-phone:text-[47px] sm:mr-6 lg:text-[40px]">
+            Sitio Web <br />
+            en Mantenimiento
+          </h1>
+        </article>
 
-        <h1 className="block text-right font-titleFont text-[29px] leading-[1.2] xs-mini-phone:text-[35px] xs:text-[40px] xs-phone:text-[45px] sm:hidden">
-          Sitio Web
-          <br />
-          en Mantenimiento
-        </h1>
-
-        {/* Párrafo para pantallas grandes */}
-        <p className="lg:pt-[296px ] hidden text-right font-textFont text-[28px] sm:block sm:pr-4 sm:text-[25px] md:text-[30px] lg:pr-[185px] lg:text-[30px]">
-          Disculpe las molestias
-          <br />
-          estamos trabajando
-          <br />
-          para mejorar el servicio.
-        </p>
-
-        {/* Sección en mantenimiento */}
-        {section && (
-          <p className="text-right text-lg font-semibold sm:pr-4 lg:pr-[160px]">
-            La sección <span className="text-blue-500 dark:text-blue-300">{section}</span> está en mantenimiento.
-          </p>
-        )}
-
-        {/* Redirección */}
-        <div className="hidden w-full text-right sm:block sm:pr-4 lg:pr-[185px]">
-          <Link href="/">
-            <div className="inline-flex w-[165px] items-center justify-center rounded-full border border-buttonsLigth bg-transparent p-2 text-base font-bold text-buttonsLigth dark:border-darkText dark:text-darkText">
-              Redirección en {counter}
-            </div>
-          </Link>
-        </div>
-      </section>
-
-      {/* Contenedor de la imagen */}
-      <section className="relative flex w-full items-end xs:min-h-[650px] md:min-h-[600px] lg:min-h-[740px]">
-        <article className="absolute top-0 flex w-full flex-col items-center justify-center sm:left-[80px] sm:top-[80px] md:left-[100px] md:top-[120px] lg:left-[180px] lg:top-[160px]">
-          {/* Imagen para pantallas grandes */}
+        <article className="flex w-full flex-col items-center gap-3 md-phone:absolute">
           <Image
             src={Mantenimiento}
-            className="hidden sm:block sm:max-w-[500px] md:max-w-[600px] lg:max-w-[750px]"
+            className="max-w-[310px] xs-mini-phone:min-w-[320px] xs-mini-phone2:min-w-[330px] xs:min-w-[350px] xs-phone:max-w-[400px] sm:max-w-[430px] md-phone:absolute md-phone:right-[158px] md-phone:top-[169px] md-phone:max-w-[500px] lg-tablet:right-[250px] lg-tablet:min-w-[600px] lg2:right-[320px]"
             alt="Descripción de la imagen mantenimiento"
             width={1000}
             height={400}
           />
-
-          {/* Imagen para pantallas pequeñas */}
-          <Image
-            src={Mantenimiento}
-            className="block px-4 sm:hidden"
-            alt="Descripción de la imagen mantenimiento"
-            width={600}
-            height={400}
-          />
-
-          {/* Párrafo para pantallas pequeñas */}
-          <div className="w-full text-right xs-mini-phone:min-h-20 xs-mini-phone2:mr-6 xs:mr-5 sm:hidden">
-            <p className="titleFont mb-5 w-full text-right text-[27px] xs-mini-phone:mr-4 xs-mini-phone:text-[30px] xs-mini-phone2:text-[33px] xs:mr-7 xs:text-[36px] xs-phone:text-[43px] sm:hidden">
-              Disculpe las molestias,
-              <br />
-              estamos trabajando
-              <br />
-              para mejorar el servicio.
-            </p>
-
-            {/* Botón de redirección para pantallas pequeñas */}
-
-            <Link href="/">
-              <div className="inline-flex w-[165px] items-center justify-center rounded-full border border-buttonsLigth bg-transparent p-2 text-base font-bold text-buttonsLigth dark:border-darkText dark:text-darkText">
-                Redirección en {counter}
-              </div>
-            </Link>
-          </div>
+        </article>
+        <article className="flex w-full flex-col items-end justify-center xs:mr-3 xs-phone:mr-5 md-phone:z-20 md-phone:text-right lg-tablet:absolute lg-tablet:right-[135px] lg-tablet:top-44 lg2:right-[210px]">
+          {' '}
+          {/* Texto único */}
+          <p className="titleFont text-right text-[26px] xs-mini-phone:text-[30px] xs-mini-phone2:text-[32px] xs:text-[34px] xs-phone:text-[38px] md-phone:text-[32px]">
+            Disculpe las molestias,
+            <br />
+            estamos trabajando
+            <br />
+            para mejorar el servicio.
+          </p>
+          {/* Sección en mantenimiento */}
+          {section && <p className="mt-2">La sección {section} está en mantenimiento.</p>}
+          {/* Botón de redirección */}
+          <RedirectButton counter={counter} />
         </article>
       </section>
     </section>
