a3df9078a7493c7a10663e54df9f533e336bbfdb
Lautaro <lautarogandodev@gmail.com>
2025-01-31 12:55:49 -0300

    fix: vista de editar solicitud finalizada

--- components/EditSolicitud/HeaderSectionEditSoli/HeaderSectionEditSoli.tsx
+++ components/EditSolicitud/HeaderSectionEditSoli/HeaderSectionEditSoli.tsx
@@ -3,8 +3,8 @@ import ImageSectionEditSoli from '../ImageSectionEditSoli/ImageSectionEditSoli';
 
 const HeaderSectionEditSoli = () => {
   return (
-    <div className="flex w-full flex-col lg:flex-row">
-      <div className="flex w-full flex-col flex-wrap content-center px-0 md:px-10 lg:px-0">
+    <div className="flex w-full flex-col lg:flex-row lg2:w-0">
+      <div className="mx-auto flex w-full flex-col flex-wrap content-center px-0 md:max-w-[506px] lg:px-0 lg2:absolute lg2:top-0 lg2:h-[424px] lg2:w-[384px]">
         <ImageSectionEditSoli />
       </div>
     </div>
