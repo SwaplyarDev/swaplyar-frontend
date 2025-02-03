f3c5a4c3814c914fa0610b3d25058dd6ce8dc1d7
Ignacio Lopez <nacholopez1903@gmail.com>
2025-02-03 13:04:20 -0300

    feat: actualizando stepThree

--- components/request/form/steps/stepsThreeOptions/StepThreeGeneral.tsx
+++ components/request/form/steps/stepsThreeOptions/StepThreeGeneral.tsx
@@ -4,6 +4,7 @@ import { FieldErrors, UseFormGetValues, UseFormRegister } from 'react-hook-form'
 import InputCopy from '../../inputs/InputCopy';
 import InputField from '@/components/ui/contact-form/InputField';
 import { System } from '@/types/data';
+import { useDarkTheme } from '@/components/ui/theme-Provider/themeProvider';
 
 interface StepThreeGeneralProps {
   register: UseFormRegister<any>;
@@ -31,8 +32,20 @@ const StepThreeGeneral: React.FC<StepThreeGeneralProps> = ({
   handleChange,
   restRegister,
 }) => {
-  console.log(' selectedSendingSystem: ', selectedSendingSystem);
-  console.log(' selectedReceivingSystem: ', selectedReceivingSystem);
+  const { isDark } = useDarkTheme();
+  const [file, setFile] = React.useState<File | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files?.[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+    }
+  };
+
+  const handleFileRemove = () => {
+    setFile(null);
+  };
+
   return (
     <>
       <p className="text-left">
@@ -112,6 +125,27 @@ const StepThreeGeneral: React.FC<StepThreeGeneralProps> = ({
               error={errors.receive_amount && 'Este campo es obligatorio'}
             />
           </div>
+          <div className="flex h-full flex-col">
+            <label
+              htmlFor="note"
+              className={clsx('ml-1 h-5 text-xs', errors.note ? 'text-red-500' : 'text-lightText dark:text-darkText')}
+            >
+              Nota (opcional)
+            </label>
+            <textarea
+              {...register('note', { required: false })}
+              id="note"
+              disabled={blockAll}
+              placeholder="Añade una nota si lo deseas ;)"
+              className={clsx(
+                'h-full w-full rounded border bg-gray-200 px-5 py-2 dark:bg-lightText',
+                errors.note ? 'border-red-500' : 'hover:border-blue-600 dark:hover:border-white',
+              )}
+            ></textarea>
+            {errors.note && <p className="text-sm text-red-500">Este campo es obligatorio</p>}
+          </div>
+        </div>
+        <div className="flex w-full flex-col gap-4">
           <div className="flex flex-col">
             <label
               htmlFor="proof_of_payment"
@@ -121,18 +155,45 @@ const StepThreeGeneral: React.FC<StepThreeGeneralProps> = ({
               )}
             >
               <p className="ml-1 h-5 text-xs">Comprobante con formato: PNG o JPG</p>
-              <div
-                className={clsx(
-                  'flex w-full flex-col items-center gap-2 rounded border border-[#6B7280] bg-gray-200 px-5 py-2 dark:bg-lightText xs-phone:flex-row sm-phone:flex-col lg-tablet:h-[38px] lg-tablet:flex-row lg-tablet:items-center',
-                  errors.proof_of_payment ? 'border-red-500' : 'hover:border-blue-600 dark:hover:border-white',
-                )}
-              >
-                <p className="w-full text-sm xs-phone:max-w-32 sm-phone:max-w-[inherit] lg-tablet:max-w-40 lg-tablet:text-base">
-                  Subir comprobante
-                </p>
-                <span id="file-name" className="w-full text-xs text-[#6B7280] lg-tablet:text-sm">
-                  No hay archivo seleccionado
-                </span>
+              <div className="file-upload flex flex-col justify-between rounded-2xl">
+                <div className="group flex h-full w-full flex-col items-center justify-center rounded-2xl border-[1px] border-inputLightDisabled py-2 text-center hover:border-inputLight dark:border-disabledDarkText dark:border-transparent dark:text-lightText dark:hover:border-darkText">
+                  {!file ? (
+                    <>
+                      <p className="mb-1 hidden text-sm text-inputLightDisabled group-hover:text-buttonsLigth dark:text-disabledDarkText group-hover:dark:text-darkText lg:inline-block">
+                        SUBIR COMPROBANTE
+                      </p>
+                      <label
+                        className={`${isDark ? 'buttonSecondDark' : 'buttonSecond'} relative mt-5 h-[48px] w-full max-w-[200px] items-center justify-center rounded-3xl border border-disabledButtonsLigth bg-disabledButtonsLigth p-[10px] text-lg text-darkText group-hover:border-buttonsLigth group-hover:bg-buttonsLigth group-hover:text-darkText dark:border-disabledButtonsDark dark:bg-disabledButtonsDark dark:text-darkText group-hover:dark:border-darkText group-hover:dark:bg-darkText group-hover:dark:text-lightText lg:mt-0`}
+                      >
+                        Subir Archivo
+                        <input
+                          type="file"
+                          onChange={handleFileChange}
+                          accept=".png,.jpg,.pdf"
+                          className="absolute inset-0 h-full w-full cursor-pointer opacity-0"
+                        />
+                      </label>
+                    </>
+                  ) : (
+                    <div className="flex items-center">
+                      <p className="text-sm text-buttonsLigth group-hover:text-darkText dark:text-disabledDarkText">
+                        Archivo seleccionado: {file.name}
+                      </p>
+                      <button
+                        onClick={handleFileRemove}
+                        className="ml-3 rounded-full bg-buttonsLigth px-2 text-darkText hover:bg-errorColor dark:bg-darkText dark:text-lightText hover:dark:bg-errorColor"
+                        aria-label="Eliminar archivo"
+                      >
+                        X
+                      </button>
+                    </div>
+                  )}
+                  {!file && (
+                    <p className="mt-1 text-xs text-inputLightDisabled group-hover:text-buttonsLigth dark:text-disabledDarkText group-hover:dark:text-darkText">
+                      Formatos de archivo: PNG, JPG, PDF
+                    </p>
+                  )}
+                </div>
               </div>
             </label>
             <div className="flex h-full flex-col">
@@ -154,27 +215,6 @@ const StepThreeGeneral: React.FC<StepThreeGeneralProps> = ({
                 <p className="text-sm text-red-500">• Este campo es obligatorio</p>
               )}
             </div>
-          </div>
-        </div>
-        <div className="flex w-full flex-col gap-4">
-          <div className="flex h-full flex-col">
-            <label
-              htmlFor="note"
-              className={clsx('ml-1 h-5 text-xs', errors.note ? 'text-red-500' : 'text-lightText dark:text-darkText')}
-            >
-              Nota (opcional)
-            </label>
-            <textarea
-              {...register('note', { required: false })}
-              id="note"
-              disabled={blockAll}
-              placeholder="Añade una nota si lo deseas ;)"
-              className={clsx(
-                'h-full w-full rounded border bg-gray-200 px-5 py-2 dark:bg-lightText',
-                errors.note ? 'border-red-500' : 'hover:border-blue-600 dark:hover:border-white',
-              )}
-            ></textarea>
-            {errors.note && <p className="text-sm text-red-500">Este campo es obligatorio</p>}
           </div>
         </div>
       </div>
