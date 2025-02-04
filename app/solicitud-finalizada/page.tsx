c4ca06b9e1e00ffe521a33451f3b77e92fcf9f4a
marcosfeijoo <marcosfeijoo97@gmail.com>
2025-02-04 09:30:42 -0300

    feat: diseñando pagina segun el figma

---
+++ app/solicitud-finalizada/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SolicitudFinalizada from '@/components/SolicitudFinalizada/SolicitudFinalizada';
+
+const page = () => {
+  return (
+    <div>
+      <SolicitudFinalizada />
+    </div>
+  );
+};
+
+export default page;
