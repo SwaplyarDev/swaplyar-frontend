68a11368896fc9c8715fccb76caddce1aa0e8c23
Lautaro <lautarogandodev@gmail.com>
2025-02-14 10:45:27 -0300

    fix: se corrigio el formulario al enviar la solicitud

--- components/modals/ModalTipos.tsx
+++ components/modals/ModalTipos.tsx
@@ -154,12 +154,12 @@ const Modal1: React.FC<ModalProps> = ({ isOpen, onClose, isDark, transaccionId }
           <div className="flex flex-col text-start text-lightText dark:text-darkText">
             <p>Nombre </p>
             <p>Apellido </p>
-            <p>Email </p>
+            <p>Correo electrónico </p>
           </div>
           <div className="flex flex-col text-end text-lightText dark:text-darkText">
-            <p>{transactionData?.transaction?.receiver?.first_name}</p>
-            <p>{transactionData?.transaction?.receiver?.last_name}</p>
-            <p>{transactionData?.transaction?.receiver?.email}</p>
+            <p>{transactionData.receiver.first_name}</p>
+            <p>{transactionData.receiver.last_name}</p>
+            <p>{transactionData.payment_method.receiver.details.email_account}</p>
           </div>
         </div>
       );
@@ -171,8 +171,8 @@ const Modal1: React.FC<ModalProps> = ({ isOpen, onClose, isDark, transaccionId }
             <p>Red </p>
           </div>
           <div className="flex flex-col text-end text-lightText dark:text-darkText">
-            <p>{transactionData?.transaction?.receiver?.payment_method_id}</p>
-            <p>{transactionData?.transaction?.country_transaction}</p>
+            <p>{transactionData.receiver.payment_method_id}</p>
+            <p>{transactionData.transaction.country_transaction}</p>
           </div>
         </div>
       );
@@ -187,9 +187,9 @@ const Modal1: React.FC<ModalProps> = ({ isOpen, onClose, isDark, transaccionId }
             <p>CBU/CVU/ALIAS</p>
           </div>
           <div className="flex flex-col text-end text-lightText dark:text-darkText">
-            <p>{transactionData?.transaction?.sender?.first_name}</p>
-            <p>{transactionData?.transaction?.sender?.last_name}</p>
-            <p>{transactionData?.transaction?.sender?.identification}</p>
+            <p>{transactionData.sender.first_name}</p>
+            <p>{transactionData.sender.last_name}</p>
+            <p>{transactionData.sender.identification}</p>
             <p>{transactionData?.transaction?.payment_method?.value}</p>
             <p>{transactionData?.transaction?.payment_method?.value}</p>{' '}
           </div>
@@ -205,10 +205,10 @@ const Modal1: React.FC<ModalProps> = ({ isOpen, onClose, isDark, transaccionId }
             <p>Individual TAX ID (CPF)</p>
           </div>
           <div className="flex flex-col text-end text-lightText dark:text-darkText">
-            <p>{transactionData?.transaction?.sender?.first_name}</p>
-            <p>{transactionData?.transaction?.sender?.last_name}</p>
-            <p>{transactionData?.transaction?.sender?.identification}</p>
-            <p>{transactionData?.transaction?.receiver?.payment_method_id}</p>
+            <p>{transactionData.sender.first_name}</p>
+            <p>{transactionData.sender.last_name}</p>
+            <p>{transactionData.sender.identification}</p>
+            <p>{transactionData.receiver.payment_method_id}</p>
           </div>
         </div>
       );
@@ -217,6 +217,7 @@ const Modal1: React.FC<ModalProps> = ({ isOpen, onClose, isDark, transaccionId }
       return null;
     }
   };
+  console.log(transactionData);
   return (
     <div className="fixed inset-0 left-0 right-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div
@@ -227,71 +228,56 @@ const Modal1: React.FC<ModalProps> = ({ isOpen, onClose, isDark, transaccionId }
         <button onClick={onClose} className="absolute right-2.5 top-1.5 text-2xl">
           <p className={`inline-block text-buttonsLigth dark:text-darkText`}>X</p>
         </button>
-        <h2 className="mb-4 pl-4 pr-7 text-start text-lg font-bold text-lightText dark:text-darkText">
-          Formulario de solicitud N°{transaccionId}
+        <h2 className="mb-4 pl-4 pr-7 text-start font-textFont text-lg font-semibold text-lightText dark:text-darkText">
+          Formulario de Solicitud N° {transaccionId}
         </h2>
 
-        <div className="max-h-[70vh] overflow-y-auto px-4 scrollbar-thin">
-          <section className="flex flex-col">
-            <h3 className="mb-2 flex text-base font-semibold text-buttonsLigth dark:text-darkText">Mis Datos</h3>
+        <div className="flex max-h-[70vh] flex-col gap-2 overflow-y-auto px-4 scrollbar-thin">
+          <section className="flex flex-col border-b border-darkText pb-2">
+            <h3 className="mb-2 flex font-textFont font-semibold text-buttonsLigth dark:text-darkText">Mis Datos</h3>
             <div className="flex justify-between text-sm">
-              <div className="flex flex-col text-start text-lightText dark:text-darkText">
+              <div className="flex flex-col text-start font-textFont font-light text-lightText dark:text-darkText">
                 <p>Nombre </p>
                 <p>Apellido </p>
                 <p>Email </p>
                 <p>N° de Teléfono </p>
               </div>
-              <div className="flex flex-col text-end text-lightText dark:text-darkText">
-                <p>{transactionData?.transaction?.sender?.first_name}</p>
-                <p>{transactionData?.transaction?.sender?.last_name}</p>
-                <p>{transactionData?.transaction?.sender?.email}</p>
-                <p>{transactionData?.transaction?.sender?.phone_number}</p>
+              <div className="flex flex-col text-end font-textFont font-light text-lightText dark:text-darkText">
+                <p>{transactionData.sender.first_name}</p>
+                <p>{transactionData.sender.last_name}</p>
+                <p>{transactionData.sender.email}</p>
+                <p>{transactionData.sender.phone_number}</p>
               </div>
             </div>
           </section>
-          <div className="my-3 flex w-full justify-center">
-            <div
-              className={`flex h-full w-full border-0 border-b-buttonsLigth dark:border-b-darkText lg:border-b-2`}
-            ></div>
-          </div>
 
-          <section className="flex flex-col">
-            <h3 className="mb-2 flex text-base font-semibold text-buttonsLigth dark:text-darkText">
+          <section className="flex flex-col border-b border-darkText pb-2">
+            <h3 className="mb-2 flex font-textFont font-semibold text-buttonsLigth dark:text-darkText">
               informacion del Destinatario
             </h3>
 
             <PayMethodInfo methodDestinatario={methodDestinatario} />
           </section>
-          <div className="my-3 flex w-full justify-center">
-            <div
-              className={`flex h-full w-full border-0 border-b-buttonsLigth dark:border-b-darkText lg:border-b-2`}
-            ></div>
-          </div>
 
-          <section className="flex flex-col">
-            <h3 className="mb-2 flex text-base font-semibold text-buttonsLigth dark:text-darkText">Pago</h3>
+          <section className="flex flex-col border-b border-darkText pb-2">
+            <h3 className="mb-2 flex font-textFont font-semibold text-buttonsLigth dark:text-darkText">Pago</h3>
             <div className="flex justify-between text-sm">
               <div className="flex flex-col text-start text-lightText dark:text-darkText">
                 <p className="mr-20">Monto a pagar </p>
                 <p>Monto a recibir </p>
               </div>
               <div className="flex flex-col text-end text-lightText dark:text-darkText">
-                <p>{transactionData?.transaction?.amounts?.sent?.amount}</p>
-                <p>{transactionData?.transaction?.amounts?.received?.amount}</p>
+                <p>{transactionData.amounts.sent.amount}</p>
+                <p>{transactionData.amounts.received.amount}</p>
               </div>
             </div>
           </section>
-          <div className="mb-6 mt-3 flex w-full justify-center">
-            <div
-              className={`flex h-full w-full border-0 border-b-buttonsLigth dark:border-b-darkText lg:border-b-2`}
-            ></div>
-          </div>
 
           <section>
             <p className="mb-5 flex h-1/2 flex-col text-buttonsLigth dark:text-darkText">
-              <div className="text-start text-sm">
-                <span className="mr-1 font-bold">Nota:</span>
-                <span className="text-start">
+              <div className="text-start font-textFont text-sm">
+                <span className="mr-1 font-semibold">Nota:</span>
+                <span className="text-start text-xs font-light">
                   Indique la sección que desea modificar o cargue el comprobante correcto si envió uno por error.{' '}
                 </span>
               </div>
@@ -300,7 +286,7 @@ const Modal1: React.FC<ModalProps> = ({ isOpen, onClose, isDark, transaccionId }
               <label
                 htmlFor="note"
                 className={clsx(
-                  'text-lightText dark:text-darkText',
+                  'font-textFont font-light text-lightText dark:text-darkText',
                   !isFocused && 'hidden',
                   'w-full pl-3 text-start text-sm',
                 )}
@@ -310,11 +296,12 @@ const Modal1: React.FC<ModalProps> = ({ isOpen, onClose, isDark, transaccionId }
               <textarea
                 rows={1}
                 id="note"
-                className={`mb-4 block w-full rounded-2xl border border-inputLightDisabled px-3 py-2 placeholder-inputLightDisabled hover:border-inputLight hover:placeholder-inputLight focus:placeholder-transparent dark:border-transparent dark:text-lightText dark:placeholder-placeholderDark dark:hover:border-lightText hover:dark:border-transparent dark:hover:placeholder-lightText focus:dark:border-transparent focus:dark:ring-transparent`}
+                className={`w-ful mb-4 block h-[45px] resize-none rounded-2xl border border-inputLightDisabled px-3 py-2 placeholder-inputLightDisabled hover:border-inputLight hover:placeholder-inputLight focus:placeholder-transparent dark:border-transparent dark:text-lightText dark:placeholder-placeholderDark dark:hover:border-lightText hover:dark:border-transparent dark:hover:placeholder-lightText focus:dark:border-transparent focus:dark:ring-transparent`}
                 placeholder={isFocused ? '' : 'Necesito Modificar...'}
                 onChange={handleNoteChange}
                 required
                 minLength={20}
+                maxLength={200}
                 onFocus={() => setIsFocused(true)}
                 onBlur={(e) => setIsFocused(e.target.value !== '')}
               ></textarea>
@@ -323,13 +310,13 @@ const Modal1: React.FC<ModalProps> = ({ isOpen, onClose, isDark, transaccionId }
                 <div className="group flex h-full w-full flex-col items-center justify-center rounded-2xl border-[1px] border-inputLightDisabled py-2 text-center hover:border-inputLight dark:border-disabledDarkText dark:border-transparent dark:text-lightText dark:hover:border-darkText">
                   {!file ? (
                     <>
-                      <p className="mb-1 hidden text-sm text-inputLightDisabled group-hover:text-buttonsLigth dark:text-disabledDarkText group-hover:dark:text-darkText lg:inline-block">
+                      <p className="mb-1 hidden font-textFont text-sm text-inputLightDisabled group-hover:text-buttonsLigth dark:text-disabledDarkText group-hover:dark:text-darkText lg:inline-block">
                         SUBIR COMPROBANTE
                       </p>
                       <label
                         className={`${
                           isDark ? 'buttonSecondDark' : 'buttonSecond'
-                        } relative mt-5 h-[48px] w-full max-w-[200px] items-center justify-center rounded-3xl border border-disabledButtonsLigth bg-disabledButtonsLigth p-[10px] text-lg text-darkText group-hover:border-buttonsLigth group-hover:bg-buttonsLigth group-hover:text-darkText dark:border-disabledButtonsDark dark:bg-disabledButtonsDark dark:text-darkText group-hover:dark:border-darkText group-hover:dark:bg-darkText group-hover:dark:text-lightText lg:mt-0`}
+                        } relative mt-5 h-[48px] w-full max-w-[200px] items-center justify-center rounded-3xl border border-disabledButtonsLigth bg-disabledButtonsLigth p-[10px] font-textFont text-lg text-darkText group-hover:border-buttonsLigth group-hover:bg-buttonsLigth group-hover:text-darkText dark:border-disabledButtonsDark dark:bg-disabledButtonsDark dark:text-darkText group-hover:dark:border-darkText group-hover:dark:bg-darkText group-hover:dark:text-lightText lg:mt-0`}
                       >
                         Subir Archivo
                         <input
@@ -367,9 +354,7 @@ const Modal1: React.FC<ModalProps> = ({ isOpen, onClose, isDark, transaccionId }
           <div className="mt-4 flex flex-col-reverse justify-between xs:flex-row">
             <button
               onClick={onClose}
-              className={`${
-                isDark ? 'buttonSecondDark' : 'buttonSecond'
-              } group relative m-1 flex h-[48px] min-w-[112px] items-center justify-center gap-2 rounded-3xl border border-buttonsLigth p-3 text-buttonsLigth dark:border-darkText dark:text-darkText xs:min-w-[1px]`}
+              className={`group relative m-1 flex h-[48px] min-w-[112px] items-center justify-center gap-2 rounded-3xl border border-buttonsLigth p-3 text-xl font-light text-buttonsLigth dark:border-darkText dark:text-darkText xs:min-w-[1px]`}
             >
               <div className="relative h-5 w-5 overflow-hidden">
                 <div className="absolute left-0 transition-all ease-in-out group-hover:left-1">
@@ -391,7 +376,7 @@ const Modal1: React.FC<ModalProps> = ({ isOpen, onClose, isDark, transaccionId }
               type="button"
             >
               {loading ? (
-                <div id="loading" className="flex items-center justify-center gap-2">
+                <div id="loading" className="flex items-center justify-center gap-2 font-titleFont font-semibold">
                   <LoadingGif color={!isDark ? '#ebe7e0' : '#012c8a'} />
                   <span>Enviando...</span>
                 </div>
