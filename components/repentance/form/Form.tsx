bf8a270725a87eb3db80cdc21747b3e973f0ed50
Lautaro <lautarogandodev@gmail.com>
2025-02-24 09:42:53 -0300

    fix: se dejo la imagen estatica y se establecio un limite de caracteres

--- components/repentance/form/Form.tsx
+++ components/repentance/form/Form.tsx
@@ -211,6 +211,7 @@ const Form: React.FC<FormularioProps> = ({ onSubmit }) => {
           Nota Opcional{' '}
           <textarea
             {...register('note')}
+            maxLength={200}
             className={clsx(
               'placeholder-text-gray-900 h-[45px] max-h-[148px] min-h-[45px] w-full border-0 border-b-[1px] border-solid ps-0 text-xs placeholder:font-light xs:text-lg',
               isDark
