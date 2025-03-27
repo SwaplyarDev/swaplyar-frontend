1de78eb298cb9b37f317506f69b8e63f0f05649e
Santiago Navarro <santinavarro2003@gmail.com>
2025-03-27 11:54:08 -0300

    refactor: topmenu sin imports y sin codigo sin uso

--- components/ui/top-menu/TopMenu.tsx
+++ components/ui/top-menu/TopMenu.tsx
@@ -2,14 +2,8 @@
 
 import TopPopUp from './topPopUp';
 import NavbarLanding from '@/components/Header/Navbar/NavbarLanding';
-import NavbarInternal from '@/components/Header/NavbarInternal/NavbarInternal';
-import { useSession } from 'next-auth/react';
 
 export function TopMenu() {
-  const { data: session, status } = useSession();
-
-  // Hasta aca, sacado de MUI
-
   return (
     <main className={`sticky top-0 z-[1000] flex flex-col bg-white shadow-md dark:bg-lightText`}>
       <TopPopUp />
