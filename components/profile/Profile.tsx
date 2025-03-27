ed8d096682055aa2ef9865be00ea090121880d17
Santiago Navarro <santinavarro2003@gmail.com>
2025-03-27 12:58:17 -0300

    feat: creacion componente de perfil de usuario

---
+++ components/profile/Profile.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+const Profile = () => {
+  return (
+    <div>
+      <div>
+        <h1>Perfil</h1>
+        <div>
+          <p>Nombre Usuario</p>
+          <p>Regustrado en: 2019</p>
+        </div>
+        <Image src="" height={100} width={100} alt="imagen de perfil" />
+      </div>
+      <div>
+        {/*Card info presonal*/}
+        {/* Card correo electronico */}
+        {/* Card Whatsapp */}
+        {/* Card Redes Sociales */}
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
