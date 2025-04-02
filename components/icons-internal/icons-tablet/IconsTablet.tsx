732cdc8632f171e88614fea24cff9a4b58a84c43
tinchx1 <tinchomarchi34@gmail.com>
2025-04-02 10:27:51 -0300

    feat: finish navegation

--- components/icons-internal/icons-tablet/IconsTablet.tsx
+++ components/icons-internal/icons-tablet/IconsTablet.tsx
@@ -12,7 +12,7 @@ const IconsTablet: React.FC<IconsTabletProps> = ({ children, text, classname }) 
   const isMobile = () => (windowWidth >= 390 ? false : true);
   return (
     <div
-      className={`${classname} flex items-center rounded-full border-white bg-nav-blue dark:border-lightText dark:bg-white md:border-8`}
+      className={`${classname} flex items-center rounded-full border-white bg-nav-blue dark:border-lightText dark:bg-white xs-mini-phone:border-6`}
     >
       {children}
       {!isMobile() ? <p className="text-l mr-5 font-semibold leading-5 text-white dark:text-black">{text}</p> : null}
