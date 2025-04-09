8eb62203a258f1e86a93b209ac2e6282e1336e8c
tinchx1 <tinchomarchi34@gmail.com>
2025-04-09 11:47:04 -0300

    feat: fix height screen and details

--- app/es/(auth)/auth/layout.tsx
+++ app/es/(auth)/auth/layout.tsx
@@ -5,9 +5,7 @@ export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <NavbarInternal />
-      <main className="flex min-h-screen flex-col bg-white dark:bg-lightText">
-        <div className="flex flex-grow items-center justify-center">{children}</div>
-      </main>
+      <main className="bg-white dark:bg-lightText">{children}</main>
       <FooterInternal />
     </>
   );
