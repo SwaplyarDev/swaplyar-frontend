7322122819868669a53a1ede0a38b488e07796fd
gaston_dev <166333548+GastonFrontDev@users.noreply.github.com>
2025-04-15 09:00:39 -0300

    feat: divido el nav y agrego la mascara para lograr la transparencia que pide el diseño

--- components/auth/layout-components/NavbarInternal.tsx
+++ components/auth/layout-components/NavbarInternal.tsx
@@ -37,8 +37,10 @@ const NavbarInternal = () => {
   const isActive = pathname.split('/')[3];
 
   return (
-    <header className={`sticky top-0 z-[1000] w-full bg-white dark:bg-lightText`}>
-      <div className={`max-width-screen mx-auto md:max-w-[1300px] ${isDark ? 'montanaDark' : 'montana'}`}>
+    <header className={`sticky top-0 z-40 h-[134px] w-full`}>
+      {/* Parte de arriba blanca*/}
+
+      <div className={`max-width-screen mx-auto bg-white md:max-w-[1300px] ${isDark ? 'montanaDark' : 'montana'}`}>
         <div className="flex h-16 items-center justify-around mini-phone:mx-8 mini-phone:justify-between xs:mx-10 md:mx-20">
           <Link href="/es/auth/solicitud">
             <Image
@@ -61,38 +63,45 @@ const NavbarInternal = () => {
           </div>
         </div>
       </div>
-      <div className={`bg-nav-blue dark:bg-white`}>
-        <div className={`flex h-16 flex-grow xs:px-0 md:justify-between md:px-20`}>
-          <div className="mx-auto flex h-full w-full max-w-[1200px] items-center justify-between px-4">
-            {/* ${isDark ? 'montanaDark' : 'montana'} */}
-            <div className={`relative top-1 ml-10 flex h-full w-full xs:ml-0`}>
-              <Link
-                href="/es/auth/perfil"
-                className={`${isActive === 'perfil' ? 'bg-gradient-to-t' : ''} relative left-4 top-1 h-24 w-24 rounded-full from-[#98cf09] via-[#B614FF] to-[#092993] p-[4px] hover:bg-gradient-to-t xs:-left-1 xs:ml-5`}
-              >
-                <Image
+
+      {/* Parte de abajo azul */}
+
+      <nav className="sticky top-0 z-40 flex h-[64px] w-full flex-row overflow-visible">
+        <div className="h-full w-[500px] bg-amber-300"></div>
+        <div className="mask h-full w-[300px] bg-amber-400"></div>
+        <div className="h-full flex-1 bg-amber-500"></div>
+        {/* <div className="absolute left-[615px] top-[40px] z-50 h-[80px] w-[80px] rounded-full bg-stone-600"></div> */}
+      </nav>
+
+      {/* ${isDark ? 'montanaDark' : 'montana'} */}
+      {/* <div className={` `}>
+          <Link
+            href="/es/auth/perfil"
+            className={`${isActive === 'perfil' ? 'bg-gradient-to-t' : ''} relative left-4 top-1 h-24 w-24 rounded-full from-[#98cf09] via-[#B614FF] to-[#092993] p-[4px] hover:bg-gradient-to-t xs:-left-1 xs:ml-5`}
+          >
+             <Image
                   src={swaplyArAvatar}
                   alt="Foto perfil Usuario"
                   width={100}
                   height={100}
                   className="h-full w-full overflow-hidden rounded-full bg-white dark:bg-lightText"
                 />
-              </Link>
-              <p className="hidden pl-2 pt-4 font-sans text-white dark:text-black xs:block lg:ml-4 lg:hidden lg2:block">
-                Nombre Usuario
-              </p>
-            </div>
-            <div className="hidden lg:mr-10 lg:flex lg:max-w-[460px]">
+          </Link>
+          <p className="hidden pl-2 pt-4 font-sans text-white dark:text-black xs:block lg:ml-4 lg:hidden lg2:block">
+            Nombre Usuario
+          </p>
+        </div> */}
+      {/* <div className="hidden lg:mr-10 lg:flex lg:max-w-[460px]">
               <NavIcons />
-            </div>
-            <button onClick={() => signOut()} className="block xs:hidden lg:block">
+            </div> */}
+      {/* <button onClick={() => signOut()} className="block xs:hidden lg:block">
               <CerrarSesion />
-            </button>
-            <div onClick={() => setDrawerMenu(true)} className="hidden xs:block xs:pr-10 md:pr-0 lg:hidden">
-              <GiHamburgerMenu className="size-8 text-white dark:text-black" />
-            </div>
-            {/* Menu desplegable tablet */}
-            {drawerMenu && (
+            </button> */}
+      {/* <div onClick={() => setDrawerMenu(true)} className="hidden xs:block xs:pr-10 md:pr-0 lg:hidden">
+          <GiHamburgerMenu className="size-8 text-white dark:text-black" />
+        </div> */}
+      {/* Menu desplegable tablet */}
+      {/* {drawerMenu && (
               <Drawer
                 open={drawerMenu}
                 onClose={closeDrawerMenu}
@@ -183,10 +192,7 @@ const NavbarInternal = () => {
                   </div>
                 </Drawer.Items>
               </Drawer>
-            )}
-          </div>
-        </div>
-      </div>
+            )} */}
     </header>
   );
 };
