4e5428defa21e1018efa9416e57a3adce96cd61a
Karim Biscaiburo <kbiscaiburo@gmail.com>
2025-05-19 11:49:25 -0300

    feat: modificando obtencion de token

--- actions/Discounts/discounts.action.ts
+++ actions/Discounts/discounts.action.ts
@@ -1,10 +1,6 @@
 const NEXT_PUBLIC_BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
-import { auth } from '@/auth';
-export async function getDiscounts() {
-  const session = await auth();
 
-  const token = session?.accessToken || '';
-
+export async function getDiscounts(token: string) {
   try {
     const response = await fetch(`${NEXT_PUBLIC_BACKEND_URL}/v1/discount/user-discounts/user`, {
       method: 'GET',
@@ -16,7 +12,7 @@ export async function getDiscounts() {
     if (!response.ok) {
       throw new Error('Failed to fetch discounts');
     }
-    console.log('response', response);
+
     const data = await response.json();
     return data;
   } catch (error) {
