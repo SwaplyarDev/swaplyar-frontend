c5d38d08eb3b6b936d01e6e322fad4644509c59d
Karim Biscaiburo <kbiscaiburo@gmail.com>
2025-05-20 14:18:46 -0300

    feat: aniadiendo estados del usuario

--- components/auth-components/request-page/PlusRewardsComponents.tsx/PlusRewardsInitial.tsx
+++ components/auth-components/request-page/PlusRewardsComponents.tsx/PlusRewardsInitial.tsx
@@ -1,36 +1,104 @@
+'use client';
 import Image from 'next/image';
-import { VerifyButton } from '../VerifyButton';
 import { ImagePlusRewards } from '../ImagePlusRewards';
 import { useTransactions } from '@/components/historial/use-transactions';
 import { useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 import AmountTransactions from '../AmountTransactions';
 import HasGanadoDiezDolares from './HasGanadoDiezDolares';
-import BienvenidaVerificacion from './BienvenidaVerificacion';
-import { getDiscounts } from '@/actions/Discounts/discounts.action';
-function PlusRewardInitial() {
-  const { transactions } = useTransactions();
-  const { data: session } = useSession();
-  const [totalAmount, setTotalAmount] = useState(0);
-  const [totalTransactions, setTotalTransactions] = useState(0);
-  const [discounts, setDiscounts] = useState<Promise<any> | null>(null);
-  console.log(session);
-  console.log(transactions);
-  const hidden = transactions.length === 0 ? 'flex' : 'hidden';
+import WelcomeReward from './WelcomeReward';
+import PopUpSessionExpire from './PopUpSessionExpire';
+
+import { getDiscounts } from '@/actions/discounts/discounts.action';
+import VerifyAccount from './VerifyAccount';
+
+interface IDiscountsData {
+  user_discounts_id: string;
+  code_id: string;
+  user_id: string;
+  transactions_id: string;
+  is_used: 'FALSE' | 'TRUE';
+  created_at: string;
+  used_at: string;
+  code: string;
+  discount: string;
+}
+
+interface IDiscountsObject {
+  data: IDiscountsData[];
+}
+
+export default function PlusRewardInitial() {
+  const { data: session, status } = useSession();
+  const [discounts, setDiscounts] = useState<IDiscountsObject | null>(null);
 
   useEffect(() => {
-    const arrayAmounts = transactions.map((transaction) => Number(transaction.amounts.sent.amount));
-    const accumulatedAmount = arrayAmounts.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    setTotalAmount(accumulatedAmount);
-    setTotalTransactions(transactions.length);
-    try {
-      const descuentos = getDiscounts();
-      setDiscounts(descuentos);
-    } catch (error) {
-      console.error('Error fetching discounts:', error);
+    async function functionGetDiscounts() {
+      if (!session?.accessToken) return;
+
+      try {
+        const discountsData = await getDiscounts(session.accessToken);
+        setDiscounts(discountsData);
+      } catch (error) {
+        console.error(error);
+      }
     }
-  }, [transactions]);
-  console.log('descuentos', discounts);
+
+    functionGetDiscounts();
+  }, [session]);
+
+  // Pantalla de carga mientras se obtiene la session
+  if (status === 'loading') {
+    return <div className="flex h-screen w-full items-center justify-center">Loading...</div>;
+  }
+
+  // Session expirada
+  // TODO: mostrar el popup, a los 5seg se hace un logout, cuando sale del popup tambien, o si acepta con un boton
+  // ! No se probó la funcionalidad del popup
+  if (!session || !session.accessToken) {
+    return <PopUpSessionExpire />;
+  }
+
+  console.log(session, discounts);
+
+  // ! Continuacion del proyecto: Tengo que ver la forma de verificar mi cuenta para saber si funciona el renderizar componentes despues de estar verificado y chequear que se renderice bien el componente de solo verificar cuenta cuando el usuario no tiene descuentos
+
+  // const starsAndQuantity = await get
+
+  // const [totalAmount, setTotalAmount] = useState(0);
+  // const [totalTransactions, setTotalTransactions] = useState(0);
+
+  // const hidden = transactions.length === 0 ? 'flex' : 'hidden';
+
+  // useEffect(() => {
+
+  //   const arrayAmounts = transactions.map((transaction) => Number(transaction.amounts.sent.amount));
+  //   const accumulatedAmount = arrayAmounts.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  //   setTotalAmount(accumulatedAmount);
+  //   setTotalTransactions(transactions.length);
+
+  // try {
+  //   const descuentos = getDiscounts();
+  //   setDiscounts(descuentos);
+  // } catch (error) {
+  //   console.error('Error fetching discounts:', error);
+  // }
+
+  //   fetch(`${NEXT_PUBLIC_BACKEND_URL}/v1/discount/user-discounts/user`, {
+  //     method: 'GET',
+  //     headers: {
+  //       Authorization: `user_id ${session?.accessToken}`,
+  //       'Content-Type': 'application/json',
+  //     },
+  //   })
+  //     .then( response => response.json())
+  //     .then( data => setDiscounts(data))
+  //     .catch( error => console.log("Error fetching discounts:", error))
+
+  // }, [transactions]);
+
+  // console.log('descuentos', discounts);
+
   return (
     <section className="relative m-auto flex w-full max-w-7xl items-center">
       <section className="mx-auto flex w-full max-w-md flex-col justify-center rounded-lg p-6 font-light text-lightText dark:text-custom-whiteD xs-mini-phone:p-7 xs-phone:p-8 md-phone:p-10 md:flex-row-reverse lg:flex-col">
@@ -38,14 +106,43 @@ function PlusRewardInitial() {
           <div className="ml-24 xs-mini-phone:w-64 xs-phone:w-48 md-phone:w-52 lg:ml-0 lg:w-64">
             <ImagePlusRewards />
           </div>
+
           <article className="mb-5 text-end xs-phone:mb-6">
             <p className="align-text-top text-sm xs-mini-phone:text-base">Tu Código de Miembro:</p>
-            <h2 className="title text-3xl font-bold xs-mini-phone:text-[32px] xs-phone:text-[36px] md-phone:text-[40px]">
-              2448XPAR
-            </h2>
+
+            {session?.user.id ? (
+              <h2 className="title text-3xl font-bold xs-mini-phone:text-[32px] xs-phone:text-[36px] md-phone:text-[40px]">
+                {session.user.id.toUpperCase()}
+              </h2>
+            ) : (
+              <div className="ml-auto mt-2 flex h-4 w-[70%] animate-pulse overflow-hidden rounded-full bg-gray-300 p-3 md:h-8"></div>
+            )}
           </article>
         </article>
-        {totalTransactions === 5 && totalAmount >= 500 ? (
+
+        {
+          // CASE: Usuario no verificado con descuento de 3USD
+          session?.user.userVerification === null ? (
+            discounts?.data.some((discount) => discount.discount === '3' && discount.is_used === 'FALSE') ? (
+              <div className="flex w-[388px] flex-col items-center gap-[19px]">
+                <WelcomeReward />
+                <VerifyAccount />
+              </div>
+            ) : (
+              // CASE: Usuario no verificado sin descuentos
+              <VerifyAccount />
+            )
+          ) : // CASE: Usuario verificado con descuento de 3USD y 5USD
+          discounts?.data.some((discount) => discount.discount === '3' && discount.is_used === 'FALSE') &&
+            discounts?.data.some((discount) => discount.discount === '5' && discount.is_used === 'FALSE') ? (
+            <>Usuario Verificado con 3 y 5 usd</>
+          ) : (
+            // CASE: Usuario verificado con descuento de 5USD
+            <>Usuario Verificado solo</>
+          )
+        }
+
+        {/* {totalTransactions === 5 && totalAmount >= 500 ? (
           <HasGanadoDiezDolares />
         ) : (
           <div>
@@ -83,10 +180,8 @@ function PlusRewardInitial() {
               </article>
             )}
           </div>
-        )}
+        )} */}
       </section>
     </section>
   );
 }
-
-export default PlusRewardInitial;
