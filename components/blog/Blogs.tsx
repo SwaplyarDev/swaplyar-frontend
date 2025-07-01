7d6f0d0776c0bd97efb2214f98fd5d2167d10af5
Lorenzo Pachioli <lorenzopachioli@gmail.com>
2025-07-01 10:05:10 -0300

    fix: (los circulos en movimiento del fondo en Blog

--- components/blog/Blogs.tsx
+++ components/blog/Blogs.tsx
@@ -9,6 +9,7 @@ import { gifImage } from '@/utils/assets/img-database';
 import useBlogStore from '@/store/useBlogStore';
 import useFetchBlogs from '@/hooks/useFetchBlogs/useFetchBlogs';
 import SearchInput from '../ui/SearchInput/SearchInput';
+import AnimatedBlurredCircles from '../ui/animations/AnimatedBlurredCircles';
 import { paypalEnArg } from '@/utils/assets/imgDatabaseCloudinary';
 interface BlogProps {
   currentPage: number;
@@ -50,61 +51,64 @@ const Blog: React.FC<BlogProps> = ({ currentPage }) => {
 
   return (
     <>
-      <div className="mx-auto max-w-7xl p-4 sm:p-6 lg:px-20">
-        <h1 className="mb-4 text-left font-titleFont text-[61.04px] font-semibold leading-[73.25px] text-inputLight dark:text-darkText">
-          BLOG
-        </h1>
+      <div className="relative mx-4 flex flex-col items-center justify-center md:mx-8 lg:mx-4">
+        <AnimatedBlurredCircles tope="top-[0px]" />
+        <div className="mx-auto max-w-7xl p-4 sm:p-6 lg:px-20">
+          <h1 className="mb-4 text-left font-titleFont text-[61.04px] font-semibold leading-[73.25px] text-inputLight dark:text-darkText">
+            BLOG
+          </h1>
 
-        <ImageCarousel
-          images={blogs.map((post) => ({
-            blog_id: post.blog_id,
-            slug: post.slug,
-            title: post.title,
-            description: post.description,
-            image: post.image || paypalEnArg,
-            date: new Date().toISOString(),
-            category: post.category,
-          }))}
-        />
+          <ImageCarousel
+            images={blogs.map((post) => ({
+              blog_id: post.blog_id,
+              slug: post.slug,
+              title: post.title,
+              description: post.description,
+              image: post.image || paypalEnArg,
+              date: new Date().toISOString(),
+              category: post.category,
+            }))}
+          />
 
-        <SearchInput searchTerm={searchTerm} onSearchChange={handleSearchChange} results={filteredBlogs} />
-        {!isLoading ? (
-          blogs.length >= 1 ? (
-            <div className="mt-6 grid grid-cols-1 justify-items-center gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3">
-              {blogs.map((post) => (
-                <BlogPostCard
-                  key={post.blog_id}
-                  blog_id={post.blog_id}
-                  slug={post.slug}
-                  description={post.description}
-                  title={post.title}
-                  category={post.category}
-                  image={post.image || paypalEnArg}
-                  date={post?.date}
-                />
-              ))}
-            </div>
+          <SearchInput searchTerm={searchTerm} onSearchChange={handleSearchChange} results={filteredBlogs} />
+          {!isLoading ? (
+            blogs.length >= 1 ? (
+              <div className="mt-6 grid grid-cols-1 justify-items-center gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3">
+                {blogs.map((post) => (
+                  <BlogPostCard
+                    key={post.blog_id}
+                    blog_id={post.blog_id}
+                    slug={post.slug}
+                    description={post.description}
+                    title={post.title}
+                    category={post.category}
+                    image={post.image || paypalEnArg}
+                    date={post?.date}
+                  />
+                ))}
+              </div>
+            ) : (
+              <SkeletonLoader />
+            )
           ) : (
             <SkeletonLoader />
-          )
-        ) : (
-          <SkeletonLoader />
-        )}
-      </div>
-      {<PaginationButtons route="blog" totalPages={totalPages} isLoading={isLoading} currentPage={currentPage} />}
-      <div
-        className="mt-12 flex h-[272px] w-full flex-col items-center justify-center overflow-hidden bg-cover bg-center bg-no-repeat"
-        style={{ backgroundImage: `url(${gifImage})` }}
-      >
-        <div className="max-w-[90%] text-center font-textFont text-[21px] font-extrabold leading-loose text-darkText md:max-w-[600px]">
-          Mantente al día
+          )}
         </div>
-        <div className="max-w-[90%] text-center font-textFont text-[21px] font-extrabold leading-loose text-darkText md:max-w-[600px]">
-          Regístrate para recibir novedades en tu correo electrónico
-        </div>
+        {<PaginationButtons route="blog" totalPages={totalPages} isLoading={isLoading} currentPage={currentPage} />}
+        <div
+          className="mt-12 flex h-[272px] w-full flex-col items-center justify-center overflow-hidden bg-cover bg-center bg-no-repeat"
+          style={{ backgroundImage: `url(${gifImage})` }}
+        >
+          <div className="max-w-[90%] text-center font-textFont text-[21px] font-extrabold leading-loose text-darkText md:max-w-[600px]">
+            Mantente al día
+          </div>
+          <div className="max-w-[90%] text-center font-textFont text-[21px] font-extrabold leading-loose text-darkText md:max-w-[600px]">
+            Regístrate para recibir novedades en tu correo electrónico
+          </div>
 
-        <div className="mt-4 inline-flex h-[46px] w-[300px] items-center justify-center gap-2.5 rounded-[50px] bg-darkText px-3.5 py-3">
-          <div className="font-titleFont text-base font-semibold text-lightText">Suscribete</div>
+          <div className="mt-4 inline-flex h-[46px] w-[300px] items-center justify-center gap-2.5 rounded-[50px] bg-darkText px-3.5 py-3">
+            <div className="font-titleFont text-base font-semibold text-lightText">Suscribete</div>
+          </div>
         </div>
       </div>
     </>
