fdb3e77ff30e018470bcd81c3ba1135143bf9b12
Lorenzo Pachioli <lorenzopachioli@gmail.com>
2025-08-01 13:09:27 -0300

    fix(userAditionalInfo): agregado cartel de aviso de usuario rechazado

--- components/admin/UsersPageComponents/UserAdditionalInfo.tsx
+++ components/admin/UsersPageComponents/UserAdditionalInfo.tsx
@@ -1,8 +1,9 @@
 'use client';
 import { useState } from 'react';
-import { ChevronDown, ChevronUp } from 'lucide-react';
+import { AlertCircle, ChevronDown, ChevronUp } from 'lucide-react';
 import { User } from '@/types/user';
 import { DetailedVerificationItem } from '@/types/verifiedUsers';
+import { formatDate } from '@/utils/utils';
 
 export function UserAdditionalInfo({ user }: { user: DetailedVerificationItem }) {
   const [isExpanded, setIsExpanded] = useState(true);
@@ -33,34 +34,46 @@ export function UserAdditionalInfo({ user }: { user: DetailedVerificationItem })
           isExpanded ? 'max-h-[500px] opacity-100' : 'max-h-0 opacity-0'
         }`}
       >
+        {user.verification_status === 'rejected' && (
+          <div className="mb-4 rounded-lg border border-red-200 bg-red-50 p-4 dark:border-red-800/30 dark:bg-red-900/20">
+            <div className="flex items-start">
+              <AlertCircle className="mr-2 mt-0.5 h-5 w-5 flex-shrink-0 text-red-600 dark:text-red-500" />
+              <div>
+                <h3 className="text-sm font-medium text-red-800 dark:text-red-400">
+                  Verificación de usuario rechazada
+                </h3>
+              </div>
+            </div>
+          </div>
+        )}
         <div className="grid grid-cols-2 gap-4">
           <div>
             <p className="text-sm text-gray-500 dark:text-gray-400">Fecha de Inscripción</p>
-            <p className="font-medium dark:text-gray-200">{user.created_at}</p>
+            <p className="font-medium dark:text-gray-200">{formatDate(user.created_at)  || 'No disponible'}</p>
           </div>
           <div>
             <p className="text-sm text-gray-500 dark:text-gray-400">Nombre</p>
-            <p className="font-medium dark:text-gray-200">{user.user.firstName}</p>
+            <p className="font-medium dark:text-gray-200">{user.user.firstName || 'No disponible'}</p>
           </div>
            <div>
             <p className="text-sm text-gray-500 dark:text-gray-400">Apellido</p>
-            <p className="font-medium dark:text-gray-200">{user.user.lastName}</p>
+            <p className="font-medium dark:text-gray-200">{user.user.lastName || 'No disponible'}</p>
           </div> 
           <div>
             <p className="text-sm text-gray-500 dark:text-gray-400">Correo Electrónico</p>
-            <p className="font-medium dark:text-gray-200">{user.user.email}</p>
+            <p className="font-medium dark:text-gray-200">{user.user.email || 'No disponible'}</p>
           </div>
           <div>
             <p className="text-sm text-gray-500 dark:text-gray-400">N° de Documento</p>
-            <p className="font-medium dark:text-gray-200">{user.users_id}</p>
+            <p className="font-medium dark:text-gray-200">{user.user.identification || 'No disponible'}</p>
           </div>
            <div>
             <p className="text-sm text-gray-500 dark:text-gray-400">Fecha de Nacimiento</p>
-            <p className="font-medium dark:text-gray-200">{user.user.birthday}</p>
+            <p className="font-medium dark:text-gray-200">{user.user.birthday || 'No disponible'}</p>
           </div> 
           <div>
             <p className="text-sm text-gray-500 dark:text-gray-400">N° de Whatsapp</p>
-            <p className="font-medium dark:text-gray-200">{user.user.phone}</p>
+            <p className="font-medium dark:text-gray-200">{user.user.phone || 'No disponible'}</p>
           </div>
         </div>
       </div>
