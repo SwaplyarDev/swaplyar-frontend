86264a2cc3e835c8d5a691cb3e87a64a40b61a89
Lorenzo Pachioli <lorenzopachioli@gmail.com>
2025-08-01 13:32:24 -0300

    fix: correccion de nombre de variable a verification y funcionalidad agregada a handleSaveUserData

--- components/admin/UsersPageComponents/UserDocumentSection.tsx
+++ components/admin/UsersPageComponents/UserDocumentSection.tsx
@@ -7,29 +7,15 @@ import { UserAdditionalInfo } from './UserAdditionalInfo';
 import { UserVerificationForm } from './UserVerificationForm';
 import { User as UserType } from '@/types/user';
 import auth from '@/auth';
-import { DetailedVerificationItem } from '@/types/verifiedUsers';
+import { DetailedVerificationItem, VerificationResponse, VerifyForm } from '@/types/verifiedUsers';
+import { sendChangeStatus } from '@/actions/userVerification/verification.action';
 
-interface UserDocumentSectionProps {
-  user: Partial<UserType>;
-}
-interface VerifyForm {
-  document_front: string;
-  document_back: string;
-  selfie_image: string;
-  note_rejection?: string;
-}
 
-export function UserDocumentSection({ user }: { user: DetailedVerificationItem }) {
+export function UserDocumentSection({ verification }: { verification: DetailedVerificationItem }) {
   const [activeTab, setActiveTab] = useState('frente');
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isVerified, setIsVerified] = useState(user.verification_status === 'verified');
+  const [isVerified, setIsVerified] = useState(verification.verification_status === 'verified' || verification.verification_status === 'rejected');
   const [isExpanded, setIsExpanded] = useState(true);
-  const [verifyForm, setVerifyForm] = useState({
-    document_front: user.document_front || '',
-    document_back: user.document_back || '',
-    selfie_image: user.selfie_image || '',
-    note_rejection: user.note_rejection || '',
-  });
 
   const handleTabChange = (tab: string) => {
     setActiveTab(tab);
@@ -39,12 +25,19 @@ export function UserDocumentSection({ user }: { user: DetailedVerificationItem }
     setIsExpanded(!isExpanded);
   };
 
-  const handleSaveUserData = (userData: Partial<UserType>) => {
-    //const response = await sendVerification(userData, verifyForm); logica de envio de verificación
-    console.log('Datos del usuario guardados:', userData);
-    setTimeout(() => {
-      setIsVerified(true);
-    }, 1000);
+  const handleSaveUserData = async (verifyForm: VerifyForm) => {
+    const response = await sendChangeStatus(verification.verification_id, verifyForm);
+
+    if (response?.success) {
+      setIsModalOpen(false);
+      verification.verification_status = verifyForm.status;
+      if(verifyForm.status === 'verified' || verifyForm.status === 'rejected') {
+        setIsVerified(true);
+      }
+    } else {
+      console.error('Error al guardar los datos del usuario:', response?.message);
+    }
+    return response;
   };
 
   return (
@@ -106,8 +99,8 @@ export function UserDocumentSection({ user }: { user: DetailedVerificationItem }
           {activeTab === 'frente' && (
             <div className="relative h-48 w-full max-w-xs overflow-hidden rounded-lg border bg-gray-100 transition-all duration-300 hover:bg-gray-200 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-700/80">
               <div className="absolute inset-0 flex items-center justify-center">
-                {user.document_front ? (
-                  <Image src={user.document_front} alt="Document Front" layout="fill" objectFit="cover" />
+                {verification.document_front ? (
+                  <Image src={verification.document_front} alt="Document Front" layout="fill" objectFit="cover" />
                 ) : (
                   <FileText className="h-12 w-12 text-gray-400 dark:text-gray-500" />
                 )}
@@ -117,8 +110,8 @@ export function UserDocumentSection({ user }: { user: DetailedVerificationItem }
           {activeTab === 'dorso' && (
             <div className="relative h-48 w-full max-w-xs overflow-hidden rounded-lg border bg-gray-100 transition-all duration-300 hover:bg-gray-200 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-700/80">
               <div className="absolute inset-0 flex items-center justify-center">
-                {user.document_back ? (
-                  <Image src={user.document_back} alt="Document Back" layout="fill" objectFit="cover" />
+                {verification.document_back ? (
+                  <Image src={verification.document_back} alt="Document Back" layout="fill" objectFit="cover" />
                 ) : (
                   <FileText className="h-12 w-12 text-gray-400 dark:text-gray-500" />
                 )}
@@ -128,8 +121,8 @@ export function UserDocumentSection({ user }: { user: DetailedVerificationItem }
           {activeTab === 'foto' && (
             <div className="relative h-48 w-full max-w-xs overflow-hidden rounded-lg border bg-gray-100 transition-all duration-300 hover:bg-gray-200 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-700/80">
               <div className="absolute inset-0 flex items-center justify-center">
-                {user.selfie_image ? (
-                  <Image src={user.selfie_image} alt="Selfie" layout="fill" objectFit="cover" />
+                {verification.selfie_image ? (
+                  <Image src={verification.selfie_image} alt="Selfie" layout="fill" objectFit="cover" />
                 ) : (
                   <User className="h-12 w-12 text-gray-400 dark:text-gray-500" />
                 )}
@@ -141,14 +134,13 @@ export function UserDocumentSection({ user }: { user: DetailedVerificationItem }
         {isModalOpen && <VerificationModal onClose={() => setIsModalOpen(false)} />}
         {isVerified ? (
           <>
-            <UserAdditionalInfo user={user} />
+            <UserAdditionalInfo user={verification} />
           </>
         ) : (
           <>
             <UserVerificationForm
-              user={user}
+              verification={verification}
               onSave={handleSaveUserData}
-              onCancel={() => console.log('Operación cancelada')}
             />
           </>
         )}  
@@ -157,28 +149,4 @@ export function UserDocumentSection({ user }: { user: DetailedVerificationItem }
   );
 }
 
-const sendVerification = async (user: Partial<UserType>, verifyForm: VerifyForm) => {
-  try {
-    const session = await auth();
-    const token = session?.accessToken;
 
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/users/${user.id}/verification/upload`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(verifyForm),
-    });
-
-    if (!response.ok) {
-      throw new Error('Error al enviar la verificación');
-    }
-
-    const data = await response.json();
-    console.log('Verificación enviada:', data);
-    return data;
-  } catch (error) {
-    console.error('Error al enviar la verificación:', error);
-  }
-};
