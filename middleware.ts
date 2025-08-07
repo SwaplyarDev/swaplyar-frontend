549aec199fff3e5cacb676c1685f0a12fd3cae86
alan damian Fernandez <92124271+Alan-Fernandez@users.noreply.github.com>
2025-08-07 08:36:23 -0300

    refactor(NavbarLanding): eliminar comentarios innecesarios y habilitar botones de inicio de sesión y registro refactor(middleware): descomentar lógica de redirección para rutas específicas

--- middleware.ts
+++ middleware.ts
@@ -13,13 +13,13 @@ export default auth((req) => {
     // if (nextUrl.pathname.includes('blog')) {
     //   return NextResponse.redirect(new URL('/es/pagina-en-mantenimiento', req.url));
     // }
-    if (
-      nextUrl.pathname.includes('estado-de-solicitud') ||
-      nextUrl.pathname.includes('editar-solicitud') ||
-      nextUrl.pathname.includes('cancelacion-y-reembolso')
-    ) {
-      return NextResponse.redirect(new URL('/es/pagina-en-mantenimiento', req.url));
-    }
+    // if (
+    //   nextUrl.pathname.includes('estado-de-solicitud') ||
+    //   nextUrl.pathname.includes('editar-solicitud') ||
+    //   nextUrl.pathname.includes('cancelacion-y-reembolso')
+    // ) {
+    //   return NextResponse.redirect(new URL('/es/pagina-en-mantenimiento', req.url));
+    // }
 
     // Verificar si la ruta está bajo /admin
     const isAdminRoute = nextUrl.pathname.startsWith('/es/admin');
