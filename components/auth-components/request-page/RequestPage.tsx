816f666592209e012cf422f13cfa9eec0fc610a6
Ignacio Lopez <nacholopez1903@gmail.com>
2025-08-15 09:45:30 -0300

    feat: plus rewards implementados

--- components/auth-components/request-page/RequestPage.tsx
+++ components/auth-components/request-page/RequestPage.tsx
@@ -1,18 +1,50 @@
-import PlusRewardSection from './PlusRewardsComponents.tsx/PlusRewardsInitial';
+import auth from '@/auth';
+import PlusRewardInitial from './PlusRewardsComponents.tsx/PlusRewardsInitial';
 import TransactionCalculatorInternal from '@/components/Transaction/TransactionCalculatorInternal/TransactionCalculatorInternal';
+import { IDiscountsObject } from '@/types/discounts/discounts';
+import { getDiscounts } from '@/actions/Discounts/discounts.action';
+import { Suspense } from 'react';
 
-const RequestPage = () => {
+export default async function RequestPage() {
+  const session = await auth();
+
+  let discountsData: IDiscountsObject | null = null;
+  let errors: string[] = [];
+
+  if (!session || !session.accessToken) {
+    errors.push('No se ha podido obtener el usuario.');
+  } else {
+    try {
+      discountsData = await getDiscounts(session.accessToken);
+    } catch (error) {
+      console.log(error);
+      const errorMessage = 'No se han podido obtener los descuentos del usuario';
+      errors.push(errorMessage);
+    }
+  }
+
   return (
-    <div className="mx-auto mb-24 w-full max-w-[1000px] p-6 xs:mb-0 sm:my-6">
-      <h1 className="mb-6 text-4xl font-medium xs:mb-10">
+    <div className="mx-auto mb-20 max-h-[1680px] w-full max-w-[1200px] p-6 xs:mb-0 sm:my-6">
+      <h1 className="mb-6 pt-10 text-[34px] font-bold text-gray-800 dark:text-darkText xs:mb-8">
         Envía y recibe dinero de billeteras virtuales y criptomonedas
       </h1>
-      <section className="flex flex-col gap-4 lg:flex-row-reverse">
-        <PlusRewardSection />
+      <section className="flex flex-col gap-6 lg:flex-row-reverse">
+        <Suspense
+          fallback={
+            <div className="flex h-[331px] w-full animate-pulse items-center justify-center rounded-2xl bg-gray-200 dark:bg-custom-grayD-700 lg:h-[623px]"></div>
+          }
+        >
+          <PlusRewardInitial
+            discounts={discountsData}
+            // errors={errors}
+            userId={session?.user.id}
+            userVerification={session?.user.userVerification}
+            accessToken={session?.accessToken ?? ''}
+          />
+        </Suspense>
+        {/* // TODO: Se le deben pasar los errores (si existen) */}
         <TransactionCalculatorInternal />
       </section>
     </div>
   );
-};
-
-export default RequestPage;
+}
