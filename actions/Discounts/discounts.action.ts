6f656641beb084835ef6d2be4342951e9455391f
Lorenzo Pachioli <lorenzopachioli@gmail.com>
2025-09-02 11:23:47 -0300

    feature(discount.action): metodo agregado para modificar estado de cupon

--- actions/Discounts/discounts.action.ts
+++ actions/Discounts/discounts.action.ts
@@ -21,6 +21,30 @@ export async function getDiscounts(token: string) {
   }
 }
 
+export async function putDiscountsStatus(token: string, coupon_id: string, uuid_transacción: string) {
+  try {
+    const body = JSON.stringify({ transactionId: uuid_transacción });
+    const response = await fetch(`${NEXT_PUBLIC_BACKEND_URL}/discounts/user-discounts/${coupon_id}`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      body,
+    });
+    if (!response.ok) {
+      console.error('Response not ok:', response);
+      throw new Error('Failed to update discount status');
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching discounts:', error);
+    throw new Error('Failed to fetch discounts');
+  }
+}
+
 export async function getDiscountsByUserId(user_id: string, token: string) {
   try {
     const response = await fetch(`${NEXT_PUBLIC_BACKEND_URL}/discounts/user-discounts/${user_id}`, {
