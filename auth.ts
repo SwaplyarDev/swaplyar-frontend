b3f54106e5ed596d6f671d3c45ffe51d20465b03
LorMatias <156853697+LorMatias@users.noreply.github.com>
2025-09-05 12:37:16 -0300

    refactor: streamline token refresh logic and enhance error handling

--- auth.ts
+++ auth.ts
@@ -1,50 +1,105 @@
-import NextAuth, { Session } from 'next-auth';
-import { authConfig } from './auth.config';
+import NextAuth from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+import { authConfig } from './auth.config'; // Importamos la config base
+import { de } from 'date-fns/locale';
+
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL!;
+let isRefreshing = false;
+let refreshPromise: Promise<JWT> | null = null;
+async function refreshAccessToken(token: JWT): Promise<JWT> {
+  try {
+    const res = await fetch(`${BACKEND_URL}/token/refresh`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ refresh_token: token.refreshToken }),
+    });
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      throw data;
+    }
+    
+    const expiresIn = data.expires_in || 3600;
+
+    return {
+      ...token, 
+      accessToken: data.access_token,
+      refreshToken: data.refresh_token ?? token.refreshToken, 
+      expiresAt: Date.now() + expiresIn * 1000,
+    };
+
+  } catch (error) {
+    console.error('Error al refrescar el token de acceso:', error);
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError', 
+    };
+  }
+}
 
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
   signOut,
-  unstable_update: update,
 } = NextAuth({
+  ...authConfig,
   session: {
     strategy: 'jwt',
   },
-  pages: {
-    signIn: '/es/iniciar-sesion-o-registro',
-  },
-
   callbacks: {
-    /**
-     * Se invoca en el momento de la autenticación y en cada petición
-     * Almacena en el JWT el user payload y los tokens
-     */
     async jwt({ token, user }) {
       if (user) {
-        // Almacena el payload del usuario y los tokens en el JWT
-        token.user = user;
-        token.accessToken = user.accessToken;
-        token.refreshToken = user.refreshToken;
+        return {
+          ...token,
+          accessToken: user.accessToken,
+          refreshToken: user.refreshToken,
+          expiresAt: user.expiresAt,
+          user: user, 
+        };
       }
-      return token;
-    },
-    /**
-     * Permitimos el inicio de sesión sin bloqueos adicionales
-     */
-    async signIn() {
-      return true;
-    },
-    async redirect({ baseUrl }) {
-      return baseUrl;
+
+      
+      const remainingTimeInSeconds = ((token.expiresAt as number) - Date.now()) / 1000;
+      if (remainingTimeInSeconds > 0) {
+        return token;
+      }
+      if (isRefreshing) {
+        return await refreshPromise;
+      }
+      isRefreshing = true;
+      refreshPromise = refreshAccessToken(token);
+      
+      const refreshedToken = await refreshPromise;
+      
+      console.log('✨ Refresco completado.');
+      isRefreshing = false;
+      refreshPromise = null;
+
+      return refreshedToken;
     },
 
-    async session({ session, token }: { session: Session; token: any }) {
-      session = token;
+
+     async session({ session, token }) {
+      const userPayload = token.user as any; 
+
+      session.user = {
+        ...session.user, 
+        id: userPayload.sub, 
+        role: userPayload.role,
+        fullName: userPayload.fullName,
+        userVerification: userPayload.isValidated, 
+      };
+
+     
+      session.accessToken = token.accessToken as string;
+      session.error = token.error as string; 
+      
       return session;
     },
   },
-  ...authConfig,
 });
-
-export default auth;
+export default auth
