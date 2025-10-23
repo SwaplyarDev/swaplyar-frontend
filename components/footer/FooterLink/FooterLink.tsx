b5cbdb6eaa39a4c40b98e03af201204d5325d0dd
Francisco Javier Paredes <frandelregue@gmail.com>
2025-10-23 13:59:35 -0300

    style(footer): update style and fix bug in acordion

--- components/footer/FooterLink/FooterLink.tsx
+++ components/footer/FooterLink/FooterLink.tsx
@@ -13,7 +13,6 @@ export function FooterLink({ href, label, view }: FooterLinkProps) {
   const pathname = usePathname();
 
   const isActive = pathname === href;
-console.log(label, "lavel");
 
   return (
  
@@ -21,11 +20,7 @@ console.log(label, "lavel");
         <div className="align-center  flex justify-center">
           <p
           className={`text-[#012A8E] ${isActive && "underline"} transition-transform duration-300 ease-in-out hover:scale-105 hover:text-buttonsLigth dark:text-buttonsExtraLigthDark dark:hover:text-buttonsLigthDark`}
-            // className={`absolute font-textFont text-base text-buttonsExtraLigth transition-transform duration-300 ease-in-out hover:scale-105 hover:text-buttonsLigth dark:text-buttonsExtraLigthDark dark:hover:text-buttonsLigthDark ${
-            //   isActive
-            //     ? ' underline  border-[#012a8e]  text-buttonsLigth dark:border-[#d6e2ff] dark:text-buttonsLigthDark'
-            //     : 'font-light'
-            // } `}
+      
           >
             {label}
           </p>
