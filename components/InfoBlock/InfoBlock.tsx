456f1617fd39619edf78097523028cf1c11b0f1b
Sebastián <bichos1506@gmail.com>
2025-11-04 11:52:20 -0600

    tablet home

--- components/InfoBlock/InfoBlock.tsx
+++ components/InfoBlock/InfoBlock.tsx
@@ -17,7 +17,6 @@ interface InfoBlockProps {
   customContentWidth?: number;
   customGap?: number;
   customImageContainerWidth?: number;
-  verticalLayout?: boolean;
 }
 
 export default function InfoBlock({
@@ -33,30 +32,28 @@ export default function InfoBlock({
   customContentWidth,
   customGap,
   customImageContainerWidth,
-  verticalLayout = false,
 }: InfoBlockProps) {
   return (
     <div
       className={clsx(
-        verticalLayout ? 'flex-col lg:flex-row' : position ? 'flex-col sm:flex-row-reverse' : 'flex-col sm:flex-row',
+        position ? 'flex-col sm:flex-row-reverse' : 'flex-col sm:flex-row',
         'flex max-w-6xl items-center justify-center',
-        !customGap && 'gap-4 md:gap-2 lg:gap-4',
-        customGap && verticalLayout && 'lg:gap-4',
+        !customGap && 'gap-4 md:gap-2',
       )}
       style={customGap ? { gap: `${customGap}px` } : {}}
     >
-      <div className={`info-image-container ${customImageSpacing ? 'mb-0' : 'mb-4 md:mb-0'} flex ${customImageContainerWidth ? `w-[358px] xs-mini-phone:w-[358px] sm:w-[${customImageContainerWidth}px] lg:w-[${customImageContainerWidth}px] flex-shrink-0` : 'w-full max-w-[358px] xs-mini-phone:max-w-[358px] sm:max-w-[692px] justify-center lg:max-w-[500px]'} ${!customImageContainerWidth ? 'justify-center' : customImageAlign === 'left' ? 'justify-start' : customImageAlign === 'right' ? 'justify-end' : 'justify-center'}`}>
-        <div className={`relative ${customImageSpacing ? 'h-auto' : 'h-[400px] md:h-[350px]'} ${customImageContainerWidth && customImageWidth ? `w-[${customImageWidth}px] lg:w-[${customImageWidth}px]` : customImageContainerWidth ? `w-[358px] xs-mini-phone:w-[358px] sm:w-[${customImageContainerWidth}px] lg:w-[${customImageContainerWidth}px]` : 'w-full'} ${customImageSpacing ? 'overflow-visible' : 'overflow-hidden'} flex items-center ${customImageAlign === 'left' ? 'justify-start' : customImageAlign === 'right' ? 'justify-end' : 'justify-center'}`}>
+      <div className={`info-image-container ${customImageSpacing ? 'mb-0' : 'mb-4 md:mb-0'} flex w-full ${customImageContainerWidth ? `max-w-[${customImageContainerWidth}px]` : 'max-w-[692px] justify-center sm:max-w-[403px]'} ${!customImageContainerWidth ? 'justify-center' : ''}`}>
+        <div className={`relative ${customImageSpacing ? 'h-auto' : 'h-[400px] md:h-[350px]'} ${customImageContainerWidth ? `w-[${customImageContainerWidth}px]` : 'w-full'} ${customImageSpacing ? 'overflow-visible' : 'overflow-hidden'} flex items-center ${customImageAlign === 'left' ? 'justify-start' : customImageAlign === 'right' ? 'justify-end' : 'justify-center'}`}>
       <Image
         src={imageSrc}
         alt={imageAlt}
         width={customImageWidth || (customImageSpacing ? 258: 387)}
         height={ customImageSpacing ? 350:350}
-        className={`${customImageWidth ? `w-[${customImageWidth}px] h-[350px] flex-shrink-0` : customImageSpacing ? 'w-[258px] h-[350px]' : 'h-full max-w-[692px]'} object-contain drop-shadow-light dark:drop-shadow-darkmode ${customImageSpacing ? '' : 'scale-90 md:scale-90'}`}
+        className={`${customImageWidth ? `w-[${customImageWidth}px] h-[350px]` : customImageSpacing ? 'w-[258px] h-[350px]' : 'h-full max-w-[692px]'} object-contain drop-shadow-light dark:drop-shadow-darkmode ${customImageSpacing ? '' : 'scale-90 md:scale-90'}`}
       />
     </div>
       </div>
-      <div className={`info-content-container flex ${customContentWidth ? `w-[358px] xs-mini-phone:w-[358px] sm:w-full sm:max-w-[${customContentWidth}px] lg:w-[${customContentWidth}px] lg:max-w-[${customContentWidth}px]` : customImageSpacing ? 'w-[358px] xs-mini-phone:w-[358px] sm:w-full sm:max-w-[742px] lg:max-w-[500px]' : 'w-[358px] xs-mini-phone:w-[358px] sm:w-full sm:max-w-[355px] lg:max-w-[500px]'} flex-col gap-4`}>
+      <div className={`info-content-container flex  ${customContentWidth ? `max-w-[${customContentWidth}px]` : customImageSpacing ? 'max-w-[742px]' : 'max-w-[355px] md:max-w-[680px]'} flex-col gap-4`}>
         <h2 className="text-start font-textFont text-4xl text-custom-grayD dark:text-custom-whiteD">{title}</h2>
         <div className="info-content text-left text-base text-custom-grayD dark:text-custom-whiteD">
           {contentNode ? (
