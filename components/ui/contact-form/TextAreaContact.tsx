5b41720774fb74ef0052d8c1d1c4ca3722d31ef1
lazaronazareno <lazaronazareno@gmail.com>
2025-11-06 10:17:41 -0300

    style(helpCenter): update layout and styles for Help Center page; add FlyerTrabajo component style(contact-form): enhance styling and layout of ContactForm and TextAreaContact components

--- components/ui/contact-form/TextAreaContact.tsx
+++ components/ui/contact-form/TextAreaContact.tsx
@@ -45,10 +45,10 @@ const AuthTextarea: React.FC<AuthTextareaProps> = ({
         onFocus={() => setIsFocused(true)}
         onBlur={(e) => setIsFocused(e.target.value !== '')}
         className={[
-          'peer w-full rounded-2xl border bg-custom-whiteD-200 p-3 pt-5 resize-none font-titleFont text-lightText dark:bg-inputDark dark:text-lightText',
+          'peer w-full rounded-2xl border bg-custom-whiteD-200 p-3 pt-5 resize-none font-titleFont text-lightText dark:bg-transparent dark:text-custom-whiteD-200',
           error
-            ? '!border-errorColor text-errorColor'
-            : 'border-inputLightDisabled hover:border-inputLight focus:border-inputLight',
+            ? '!border-errorColor text-errorColor hover:!border-errorColor focus:!border-errorColor focus:!ring-errorColor'
+            : 'border-inputLightDisabled hover:border-inputLight focus:border-inputLight dark:hover:border-custom-whiteD-200 dark:focus:border-custom-whiteD-200',
         ]
           .filter(Boolean)
           .join(' ')}
@@ -59,9 +59,9 @@ const AuthTextarea: React.FC<AuthTextareaProps> = ({
         className={[
           'absolute left-3 px-1 transition-all duration-200 ease-in-out font-textFont',
           isFocused || value || defaultValue
-            ? 'top-[-0.6rem] text-[12px] opacity-100 bg-custom-whiteD-200 dark:bg-inputDark'
+            ? 'top-[-0.6rem] text-[12px] opacity-100 bg-custom-whiteD-200 dark:bg-custom-grayD-900'
             : 'top-3 text-[14px]',
-          'peer-hover:text-inputLight peer-focus:text-inputLight text-inputLightDisabled dark:text-darkText',
+          'peer-hover:text-inputLight peer-hover:dark:text-custom-whiteD-200 peer-focus:text-inputLightDisabled text-inputLightDisabled dark:text-inputLightDisabled peer-focus:dark:text-custom-whiteD-200',
           error && '!text-errorColor',
         ]
           .filter(Boolean)
@@ -71,7 +71,7 @@ const AuthTextarea: React.FC<AuthTextareaProps> = ({
       </label>
 
       {error && (
-        <span className="absolute left-3 top-[-0.6rem] text-sm text-errorColor px-1 bg-custom-whiteD-200 dark:bg-inputDark">
+        <span className="absolute left-3 top-[-0.6rem] text-sm text-errorColor px-1 bg-custom-whiteD-200 dark:bg-custom-grayD-900">
           {error}
         </span>
       )}
