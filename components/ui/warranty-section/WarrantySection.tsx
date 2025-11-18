3cc596645f37228446e4260a0a32a3d9357e6d8f
Sebastián <bichos1506@gmail.com>
2025-11-18 08:51:42 -0600

    porque usar responsiva

--- components/ui/warranty-section/WarrantySection.tsx
+++ components/ui/warranty-section/WarrantySection.tsx
@@ -14,13 +14,13 @@ const GuaranteeSection: React.FC<GuaranteeSectionProps> = ({ title, text, imageS
   return (
     <>
       <div className="rs-wrapper-v4 hidden w-full max-w-screen-lg flex-row items-center justify-center space-y-6 text-lightText dark:text-darkText md:flex">
-        <div className="container-text mr-8 w-[65%]">
+        <div className="container-text mr-8 w-[65%] h-[338px] flex flex-col justify-center">
           <h1 className="mb-2 text-pretty text-left font-titleFont text-3xl font-semibold lg:text-5xl">{title}</h1>
           {contentNode ? contentNode : <p className="text-pretty text-xl">{text}</p>}
         </div>
         <div className="h-auto flex-shrink-0">
           <Image
-            className="hero-img h-96 w-auto drop-shadow-light dark:drop-shadow-darkmode"
+            className="hero-img h-[338px] w-auto drop-shadow-light dark:drop-shadow-darkmode"
             src={imageSrc}
             alt={imageAlt}
             width={750}
@@ -28,10 +28,10 @@ const GuaranteeSection: React.FC<GuaranteeSectionProps> = ({ title, text, imageS
           />
         </div>
       </div>
-      <div className="rs-wrapper-v4 flex w-full max-w-screen-lg flex-col items-center justify-center space-y-6 md:hidden">
+      <div className="rs-wrapper-v4 flex w-full max-w-screen-lg flex-col items-center justify-center space-y-8 md:hidden">
         <h1 className="mb-2 text-pretty text-center font-titleFont text-3xl font-semibold lg:text-5xl">{title}</h1>
         <Image
-          className="hero-img h-96 w-auto drop-shadow-light dark:drop-shadow-darkmode"
+          className="hero-img h-[338px] w-auto drop-shadow-light dark:drop-shadow-darkmode"
           src={imageSrc}
           alt={imageAlt}
           width={750}
