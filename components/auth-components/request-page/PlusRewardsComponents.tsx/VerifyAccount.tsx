c356251b479e489192826eb50b943e371efe5353
lazaronazareno <lazaronazareno@gmail.com>
2025-11-27 11:39:18 -0300

    Update styles in requestPage and PlusRewardsInitial component for improved responsiveness. MemberCode Added

--- components/auth-components/request-page/PlusRewardsComponents.tsx/VerifyAccount.tsx
+++ components/auth-components/request-page/PlusRewardsComponents.tsx/VerifyAccount.tsx
@@ -1,10 +1,9 @@
 import ShortButton from '@/components/ui/NewButtons/ShortButton';
-import { VerifyButton } from '../VerifyButton';
 
 export default function VerifyAccount() {
   return (
     <div className="flex w-full flex-col items-center gap-[11px]">
-      <p className="text-center text-base font-light">
+      <p className="text-center text-base font-light font-textFont">
         <span className="text-lg font-semibold">Aún no has verificado tu cuenta.</span>
         <br />
         Verifícala ahora y obtén
--- components/auth-components/request-page/PlusRewardsComponents.tsx/WelcomeReward.tsx
+++ components/auth-components/request-page/PlusRewardsComponents.tsx/WelcomeReward.tsx
@@ -4,27 +4,27 @@ import Image from 'next/image';
 
 export default function WelcomeReward() {
   return (
-    <article className="flex flex-col xs-mini-phone:flex-row">
-      <p className="max-w-[165px] text-sm xs-mini-phone2:text-base">
+    <article className="flex flex-col xs-mini-phone:flex-row justify-between">
+      <p className="max-w-[150px] md-phone:max-w-[400px] lg:max-w-[170px] content-center text-base font-light font-textFont">
         <span>La recompensa de </span>
-        <span className="whitespace-nowrap text-lg font-bold text-custom-blue-800 dark:text-custom-whiteD xs-phone:text-xl">
+        <span className="whitespace-nowrap font-semibold text-custom-blue-800 dark:text-custom-whiteD">
           Bienvenida Express
         </span>
-        <span> de </span>
-        <span className="titleFon align-sub text-xl font-bold text-custom-blue-800 dark:text-custom-whiteD xs-mini-phone2:text-2xl xs-phone:text-3xl">
+        <span > de </span>
+        <span className="align-sub text-xl font-bold text-custom-blue-800 dark:text-custom-whiteD xs-mini-phone2:text-2xl xs-phone:text-3xl">
           $3 USD
         </span>
         <span> se aplica automáticamente en tu</span>
         <span className="whitespace-nowrap"> solicitud.</span>
       </p>
 
-      <div className="overflow-hidden max-h-[120px] xs-mini-phone:max-h-[100px] md-phone:max-h-[130px]">
+      <div className="overflow-hidden max-h-[145px]">
         <Image
           src={solicitudImage}
           alt="Rewards Character"
           width={197}
           height={145}
-          className="object-fill md-phone:w-[240px] lg:w-[260px] translate-y-[-40px] scale-75"
+          className="object-fill"
         />
       </div>
     </article>
